6f40b4048d39b99645604e217f6b4f58c3ef42ea
FangfangZhao <zhaofang123@gmail.com>
2019-01-07 23:01:48 +0800

    reset

--- src/main/webapp/assets/js/rb-components.jsx
+++ src/main/webapp/assets/js/rb-components.jsx
@@ -172,7 +172,7 @@ class RbHighbar extends React.Component {
 // ~~ 加载条
 function RbSpinner(props) {
     return <div className="rb-spinner">
-        <svg width="40px" height="40px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <svg width="40px" height="40px" viewBox="0 0 66 66" xmlns="http://-www.w3.org/2000/svg">
             <circle fill="none" strokeWidth="4" strokeLinecap="round" cx="33" cy="33" r="30" className="circle"></circle>
         </svg>
     </div>
