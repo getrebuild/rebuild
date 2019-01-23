510c716aef323bfe65c72c65edab9fac9feec2a3
devezhao-mac <zhaofang123@gmail.com>
2019-01-23 22:13:42 +0800

    Revert "eslint format"
    This reverts commit 4a9ea4db7c8252bc12a74b7e9ba3875e38eba5d3.

--- src/main/webapp/assets/js/entity/field-edit.js
+++ src/main/webapp/assets/js/entity/field-edit.js
@@ -1,105 +1,105 @@
 const wpc = window.__PageConfig
 $(document).ready(function(){
-  let dt = wpc.fieldType
-  if (dt.indexOf('(') > -1) dt = dt.match('\\((.+?)\\)')[1]
-  const extConfigOld = wpc.extConfig
+	let dt = wpc.fieldType
+	if (dt.indexOf('(') > -1) dt = dt.match('\\((.+?)\\)')[1]
+	const extConfigOld = wpc.extConfig
 	
-  const btn = $('.J_save').click(function(){
-    if (!wpc.metaId) return
-    let label = $val('#fieldLabel'),
-      comments = $val('#comments'),
-      nullable = $val('#fieldNullable'),
-      updatable = $val('#fieldUpdatable')
-    let _data = { fieldLabel:label, comments:comments, nullable:nullable, updatable:updatable }
-    _data = $cleanMap(_data)
+	const btn = $('.J_save').click(function(){
+		if (!!!wpc.metaId) return
+		let label = $val('#fieldLabel'),
+			comments = $val('#comments'),
+			nullable = $val('#fieldNullable'),
+			updatable = $val('#fieldUpdatable')
+		let _data = { fieldLabel:label, comments:comments, nullable:nullable, updatable:updatable }
+		_data = $cleanMap(_data)
 		
-    let extConfig = {}
-    $('.J_for-' + dt + ' .form-control').each(function(){
-      let k = $(this).attr('id')
-      let v = $val(this)
-      extConfig[k] = v
-    })
-    for (let k in extConfig){
-      if (extConfig[k] != extConfigOld[k]) {
-        _data['extConfig'] = JSON.stringify(extConfig)
-        break
-      }
-    }
+		let extConfig = {}
+		$('.J_for-' + dt + ' .form-control').each(function(){
+			let k = $(this).attr('id')
+			let v = $val(this)
+			extConfig[k] = v
+		})
+		for (let k in extConfig){
+			if (extConfig[k] != extConfigOld[k]) {
+				_data['extConfig'] = JSON.stringify(extConfig)
+				break
+			}
+		}
 
-    _data = $cleanMap(_data)
-    if (Object.keys(_data).length == 0){
-      location.href = '../fields'
-      return
-    }
+		_data = $cleanMap(_data)
+		if (Object.keys(_data).length == 0){
+			location.href = '../fields'
+			return
+		}
 
-    _data.metadata = { entity: 'MetaField', id: wpc.metaId }
-    _data = JSON.stringify(_data)
-    btn.button('loading')
-    $.post(rb.baseUrl +  '/admin/entity/field-update', _data, function(res){
-      if (res.error_code == 0) location.href = '../fields'
-      else rb.hberror(res.error_msg)
-    })
-  })
+		_data.metadata = { entity: 'MetaField', id: wpc.metaId }
+		_data = JSON.stringify(_data)
+		btn.button('loading')
+		$.post(rb.baseUrl +  '/admin/entity/field-update', _data, function(res){
+			if (res.error_code == 0) location.href = '../fields'
+			else rb.hberror(res.error_msg)
+		})
+	})
 	
-  $('#fieldNullable').attr('checked', $('#fieldNullable').data('o') == true)
-  $('#fieldUpdatable').attr('checked', $('#fieldUpdatable').data('o') == true)
+	$('#fieldNullable').attr('checked', $('#fieldNullable').data('o') == true)
+	$('#fieldUpdatable').attr('checked', $('#fieldUpdatable').data('o') == true)
 	
-  $('.J_for-' + dt).removeClass('hide')
+	$('.J_for-' + dt).removeClass('hide')
 	
-  let uploadNumber = [1, 5]
-  for (let k in extConfigOld) {
-    if (k == 'uploadNumber'){
-      uploadNumber = extConfigOld[k].split(',')
-      uploadNumber[0] = ~~uploadNumber[0]
-      uploadNumber[1] = ~~uploadNumber[1]
-      $('.J_minmax b').eq(0).text(uploadNumber[0])
-      $('.J_minmax b').eq(1).text(uploadNumber[1])
-    } else $('#' + k).val(extConfigOld[k])
-  }
-  $('input.bslider').slider({ value:uploadNumber }).on('change', function(e){
-    let v = e.value.newValue
-    $('.J_minmax b').eq(0).text(v[0])
-    $('.J_minmax b').eq(1).text(v[1])
-  })
+	let uploadNumber = [1, 5]
+	for (let k in extConfigOld) {
+		if (k == 'uploadNumber'){
+			uploadNumber = extConfigOld[k].split(',')
+			uploadNumber[0] = ~~uploadNumber[0]
+			uploadNumber[1] = ~~uploadNumber[1]
+			$('.J_minmax b').eq(0).text(uploadNumber[0])
+			$('.J_minmax b').eq(1).text(uploadNumber[1])
+		} else $('#' + k).val(extConfigOld[k])
+	}
+	$('input.bslider').slider({ value:uploadNumber }).on('change', function(e){
+		let v = e.value.newValue
+		$('.J_minmax b').eq(0).text(v[0])
+		$('.J_minmax b').eq(1).text(v[1])
+	})
 	
-  if (dt == 'PICKLIST'){
-    $.get(`${rb.baseUrl}/admin/field/picklist-gets?entity=${wpc.entityName}&field=${wpc.fieldName}&isAll=false`, function(res){
-      if (res.data.length == 0){
-        $('#picklist-items li').text('请添加选项'); return
-      }
-      $('#picklist-items').empty()
-      $(res.data).each(function(){ picklistItemRender(this) })
-      if (res.data.length > 5) $('#picklist-items').parent().removeClass('autoh')
-    })
+	if (dt == 'PICKLIST'){
+		$.get(`${rb.baseUrl}/admin/field/picklist-gets?entity=${wpc.entityName}&field=${wpc.fieldName}&isAll=false`, function(res){
+			if (res.data.length == 0){
+				$('#picklist-items li').text('请添加选项'); return
+			}
+			$('#picklist-items').empty()
+			$(res.data).each(function(){ picklistItemRender(this) })
+			if (res.data.length > 5) $('#picklist-items').parent().removeClass('autoh')
+		})
 		
-    $('.J_picklist-edit').click(function(){
-      rb.modal(`${rb.baseUrl}/admin/p/entity/picklist-config?entity=${wpc.entityName}&field=${wpc.fieldName}`, '配置列表选项')
-    })
-  } else if (dt == 'SERIES'){
-    $('#fieldNullable, #fieldUpdatable').attr('disabled', true)
-  }
+		$('.J_picklist-edit').click(function(){
+			rb.modal(`${rb.baseUrl}/admin/p/entity/picklist-config?entity=${wpc.entityName}&field=${wpc.fieldName}`, '配置列表选项')
+		})
+	} else if (dt == 'SERIES'){
+		$('#fieldNullable, #fieldUpdatable').attr('disabled', true)
+	}
 	
-  if (wpc.fieldBuildin == true) $('.footer .alert').removeClass('hide')
-  else $('.footer .J_action').removeClass('hide')
+	if (wpc.fieldBuildin == true) $('.footer .alert').removeClass('hide')
+	else $('.footer .J_action').removeClass('hide')
 
-  $('.J_del').click(function(){
-    if (!wpc.isSuperAdmin){ rb.hberror('仅超级管理员可删除字段'); return }
-    let alertExt = { type: 'danger', confirmText: '删除' }
-    alertExt.confirm = function(){
-      $(this.refs['rbalert']).find('.btn').button('loading')
-      let thatModal = this
-      $.post(`${rb.baseUrl}/admin/entity/field-drop?id=${wpc.metaId}`, function(res){
-        if (res.error_code == 0){
-          thatModal.hide()
-          rb.hbsuccess('字段已删除')
-          setTimeout(function(){ location.replace('../fields') }, 1500)
-        } else rb.hberror(res.error_msg)
-      })
-    }
-    rb.alert('字段删除后将无法恢复，请务必谨慎操作！确认删除吗？', '删除字段', alertExt)
-  })
+	$('.J_del').click(function(){
+		if (!wpc.isSuperAdmin){ rb.hberror('仅超级管理员可删除字段'); return }
+		let alertExt = { type: 'danger', confirmText: '删除' }
+            alertExt.confirm = function(){
+                $(this.refs['rbalert']).find('.btn').button('loading')
+				let thatModal = this
+                $.post(`${rb.baseUrl}/admin/entity/field-drop?id=${wpc.metaId}`, function(res){
+                    if (res.error_code == 0){
+						thatModal.hide()
+						rb.hbsuccess('字段已删除')
+						setTimeout(function(){ location.replace('../fields') }, 1500)
+                    } else rb.hberror(res.error_msg)
+                })
+            }
+		rb.alert('字段删除后将无法恢复，请务必谨慎操作！确认删除吗？', '删除字段', alertExt)
+	})
 })
 const picklistItemRender = function(data){
-  let item = $('<li class="dd-item" data-key="' + data.id + '"><div class="dd-handle">' + data.text + '</div></li>').appendTo('#picklist-items')
-  if (data['default'] == true) item.addClass('default').attr('title', '默认项')
+	let item = $('<li class="dd-item" data-key="' + data.id + '"><div class="dd-handle">' + data.text + '</div></li>').appendTo('#picklist-items')
+	if (data['default'] == true) item.addClass('default').attr('title', '默认项')
 }
--- src/main/webapp/assets/js/user-settings.jsx
+++ src/main/webapp/assets/js/user-settings.jsx
@@ -1,156 +1,156 @@
 $(document).ready(function(){
-  if (location.hash == '#secure') $('.nav-tabs a:eq(1)').trigger('click')
+    if (location.hash == '#secure') $('.nav-tabs a:eq(1)').trigger('click')
     
-  $('#avatar-input').html5Uploader({
-    name: 'avatar-input',
-    postUrl: rb.baseUrl + '/filex/upload?cloud=auto&type=image',
-    onClientLoad: function(e, file){
-      if (file.type.substr(0, 5) != 'image'){
-        rb.highbar('请上传图片')
-        return false
-      }
-    },
-    onSuccess:function(d){
-      d = JSON.parse(d.currentTarget.response)
-      if (d.error_code == 0){
-        let aUrl = `${rb.baseUrl}/cloud/img/${d.data}?imageView2/2/w/100/interlace/1/q/100`
-        $('.avatar img').attr({ 'src': aUrl, 'data-src': d.data })
-      } else rb.hberror(d.error_msg || '上传失败，请稍后重试')
-    }
-  })
-    
-  $('.J_email').click(()=>{ renderRbcomp(<DlgChangeEmail />) })
-  $('.J_passwd').click(()=>{ renderRbcomp(<DlgChangePasswd />) })
-    
-  $('.J_save').click(function(){
-    let fullName = $val('#fullName'),
-      avatarUrl = $('.avatar img').attr('data-src')
-    if (!fullName && !avatarUrl){ location.reload(); return }
-        
-    let _data = { metadata: { entity: 'User', id: window.__PageConfig.userid } }
-    if (fullName) _data.fullName = fullName
-    if (avatarUrl) _data.avatarUrl = avatarUrl
-    $.post(rb.baseUrl + '/app/entity/record-save', JSON.stringify(_data), function(res){
-      if (res.error_code == 0) location.reload()
-      else rb.highbar(res.error_msg)
+    $('#avatar-input').html5Uploader({
+        name: 'avatar-input',
+        postUrl: rb.baseUrl + '/filex/upload?cloud=auto&type=image',
+        onClientLoad: function(e, file){
+            if (file.type.substr(0, 5) != 'image'){
+                rb.highbar('请上传图片')
+                return false
+            }
+        },
+        onSuccess:function(d){
+            d = JSON.parse(d.currentTarget.response)
+            if (d.error_code == 0){
+                let aUrl = `${rb.baseUrl}/cloud/img/${d.data}?imageView2/2/w/100/interlace/1/q/100`
+                $('.avatar img').attr({ 'src': aUrl, 'data-src': d.data })
+            } else rb.hberror(d.error_msg || '上传失败，请稍后重试')
+        }
     })
-  })
+    
+    $('.J_email').click(()=>{ renderRbcomp(<DlgChangeEmail />) })
+    $('.J_passwd').click(()=>{ renderRbcomp(<DlgChangePasswd />) })
+    
+    $('.J_save').click(function(){
+        let fullName = $val('#fullName'),
+            avatarUrl = $('.avatar img').attr('data-src')
+        if (!fullName && !avatarUrl){ location.reload(); return }
+        
+        let _data = { metadata: { entity: 'User', id: window.__PageConfig.userid } }
+        if (fullName) _data.fullName = fullName
+        if (avatarUrl) _data.avatarUrl = avatarUrl
+        $.post(rb.baseUrl + '/app/entity/record-save', JSON.stringify(_data), function(res){
+            if (res.error_code == 0) location.reload()
+            else rb.highbar(res.error_msg)
+        })
+    })
 })
 
 // 修改密码
 class DlgChangePasswd extends RbFormHandler {
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    return (<RbModal title="更改密码" ref="dlg" disposeOnHide={true}>
-      <form>
-        <div className="form-group row">
-          <label className="col-sm-3 col-form-label text-sm-right">原密码</label>
-          <div className="col-sm-7">
-            <input type="password" className="form-control form-control-sm" data-id="oldPasswd" onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-3 col-form-label text-sm-right">新密码</label>
-          <div className="col-sm-7">
-            <input type="password" className="form-control form-control-sm" data-id="newPasswd" onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-3 col-form-label text-sm-right">重复新密码</label>
-          <div className="col-sm-7">
-            <input type="password" className="form-control form-control-sm" data-id="newPasswdAgain" onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group row footer">
-          <div className="col-sm-7 offset-sm-3" ref="btns">
-            <button className="btn btn-primary btn-space" type="button" onClick={()=>this.post()}>确定</button>
-            <a className="btn btn-link btn-space" onClick={()=>this.hide()}>取消</a>
-          </div>
-        </div>
-      </form>
-    </RbModal>)
-  }
-  post() {
-    let s = this.state
-    if (!s.oldPasswd){ rb.highbar('请输入原密码'); return }
-    if (!s.newPasswd){ rb.highbar('请输入新密码'); return }
-    if (s.newPasswd !== s.newPasswdAgain){ rb.highbar('两次输入的新密码不一致'); return }
-    let btns = $(this.refs['btns']).find('.btn').button('loading')
-    $.post(rb.baseUrl + '/settings/account/save-passwd?oldp=' + $encode(s.oldPasswd) + '&newp=' + $encode(s.newPasswd), (res)=>{
-      btns.button('reset')
-      if (res.error_code == 0){
-        this.hide()
-        rb.highbar('密码修改成功', 'success')
-      } else rb.highbar(res.error_msg)
-    })
-  }
+    constructor(props) {
+        super(props)
+    }
+    render(){
+        return (<RbModal title="更改密码" ref="dlg" disposeOnHide={true}>
+            <form>
+                <div className="form-group row">
+                    <label className="col-sm-3 col-form-label text-sm-right">原密码</label>
+                    <div className="col-sm-7">
+                        <input type="password" className="form-control form-control-sm" data-id="oldPasswd" onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-3 col-form-label text-sm-right">新密码</label>
+                    <div className="col-sm-7">
+                        <input type="password" className="form-control form-control-sm" data-id="newPasswd" onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-3 col-form-label text-sm-right">重复新密码</label>
+                    <div className="col-sm-7">
+                        <input type="password" className="form-control form-control-sm" data-id="newPasswdAgain" onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="form-group row footer">
+                    <div className="col-sm-7 offset-sm-3" ref="btns">
+                        <button className="btn btn-primary btn-space" type="button" onClick={()=>this.post()}>确定</button>
+                        <a className="btn btn-link btn-space" onClick={()=>this.hide()}>取消</a>
+                    </div>
+                </div>
+            </form>
+        </RbModal>)
+    }
+    post() {
+        let s = this.state
+        if (!!!s.oldPasswd){ rb.highbar('请输入原密码'); return }
+        if (!!!s.newPasswd){ rb.highbar('请输入新密码'); return }
+        if (s.newPasswd !== s.newPasswdAgain){ rb.highbar('两次输入的新密码不一致'); return }
+        let btns = $(this.refs['btns']).find('.btn').button('loading')
+        $.post(rb.baseUrl + '/settings/account/save-passwd?oldp=' + $encode(s.oldPasswd) + '&newp=' + $encode(s.newPasswd), (res)=>{
+            btns.button('reset')
+            if (res.error_code == 0){
+                this.hide()
+                rb.highbar('密码修改成功', 'success')
+            } else rb.highbar(res.error_msg)
+        })
+    }
 }
 // 修改邮箱
 class DlgChangeEmail extends RbFormHandler {
-  constructor(props) {
-    super(props)
-    this.state = { ...this.state, vcodeDisabled: false, vcodeCountdown: '获取验证码' }
-  }
-  render(){
-    return (<RbModal title="更改邮箱" ref="dlg" disposeOnHide={true}>
-      <form>
-        <div className="form-group row">
-          <label className="col-sm-3 col-form-label text-sm-right">邮箱地址</label>
-          <div className="col-sm-7">
-            <input type="text" className="form-control form-control-sm" data-id="newEmail" onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-3 col-form-label text-sm-right">验证码</label>
-          <div className="col-sm-4 pr-0">
-            <input type="text" className="form-control form-control-sm" data-id="vcode" onChange={this.handleChange} />
-          </div>
-          <div className="col-sm-3">
-            <button type="button" className="btn btn-primary bordered w-100 J_vcode" onClick={()=>this.sendVCode()} disabled={this.state.vcodeDisabled}>{this.state.vcodeCountdown}</button>
-          </div>
-        </div>
-        <div className="form-group row footer">
-          <div className="col-sm-7 offset-sm-3" ref="btns">
-            <button className="btn btn-primary btn-space" type="button" onClick={()=>this.post()}>确定</button>
-            <a className="btn btn-link btn-space" onClick={()=>this.hide()}>取消</a>
-          </div>
-        </div>
-      </form>
-    </RbModal>)
-  }
-  sendVCode() {
-    let s = this.state
-    if (!s.newEmail || !$regex.isMail(s.newEmail)){ rb.highbar('请输入有效的邮箱地址'); return }
-    this.setState({ vcodeDisabled: true })
-    $.post(rb.baseUrl + '/settings/account/send-email-vcode?email=' + $encode(s.newEmail), (res)=>{
-      if (res.error_code == 0) this.vcodeResend()
-      else rb.highbar(res.error_msg)
-    })
-  }
-  vcodeResend() {
-    let countdown = 60
-    let countdownTimer = setInterval(()=>{
-      this.setState({ vcodeCountdown: '重新获取 (' + (--countdown) + ')' })
-      if (countdown <= 0){
-        clearInterval(countdownTimer)
-        this.setState({ vcodeCountdown: '重新获取', vcodeDisabled: false })
-      }
-    }, 1000)
-  }
-  post() {
-    let s = this.state
-    if (!s.newEmail || !$regex.isMail(s.newEmail)){ rb.highbar('请输入有效的邮箱地址'); return }
-    if (!s.newEmail || !s.vcode){ rb.highbar('请输入邮箱地址和验证码'); return }
-    let btns = $(this.refs['btns']).find('.btn').button('loading')
-    $.post(rb.baseUrl + '/settings/account/save-email?email=' + $encode(s.newEmail) + '&vcode=' + $encode(s.vcode), (res)=>{
-      btns.button('reset')
-      if (res.error_code == 0){
-        this.hide()
-        $('.J_email-account').html('当前绑定邮箱 <b>' + s.newEmail + '</b>')
-        rb.highbar('邮箱修改成功', 'success')
-      } else rb.highbar(res.error_msg)
-    })
-  }
+    constructor(props) {
+        super(props)
+        this.state = { ...this.state, vcodeDisabled: false, vcodeCountdown: '获取验证码' }
+    }
+    render(){
+        return (<RbModal title="更改邮箱" ref="dlg" disposeOnHide={true}>
+            <form>
+                <div className="form-group row">
+                    <label className="col-sm-3 col-form-label text-sm-right">邮箱地址</label>
+                    <div className="col-sm-7">
+                        <input type="text" className="form-control form-control-sm" data-id="newEmail" onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-3 col-form-label text-sm-right">验证码</label>
+                    <div className="col-sm-4 pr-0">
+                        <input type="text" className="form-control form-control-sm" data-id="vcode" onChange={this.handleChange} />
+                    </div>
+                    <div className="col-sm-3">
+                        <button type="button" className="btn btn-primary bordered w-100 J_vcode" onClick={()=>this.sendVCode()} disabled={this.state.vcodeDisabled}>{this.state.vcodeCountdown}</button>
+                    </div>
+                </div>
+                <div className="form-group row footer">
+                    <div className="col-sm-7 offset-sm-3" ref="btns">
+                        <button className="btn btn-primary btn-space" type="button" onClick={()=>this.post()}>确定</button>
+                        <a className="btn btn-link btn-space" onClick={()=>this.hide()}>取消</a>
+                    </div>
+                </div>
+            </form>
+        </RbModal>)
+    }
+    sendVCode() {
+        let s = this.state
+        if (!!!s.newEmail || !$regex.isMail(s.newEmail)){ rb.highbar('请输入有效的邮箱地址'); return }
+        this.setState({ vcodeDisabled: true })
+        $.post(rb.baseUrl + '/settings/account/send-email-vcode?email=' + $encode(s.newEmail), (res)=>{
+            if (res.error_code == 0) this.vcodeResend()
+            else rb.highbar(res.error_msg)
+        })
+    }
+    vcodeResend() {
+        let countdown = 60
+        let countdownTimer = setInterval(()=>{
+            this.setState({ vcodeCountdown: '重新获取 (' + (--countdown) + ')' })
+            if (countdown <= 0){
+                clearInterval(countdownTimer)
+                this.setState({ vcodeCountdown: '重新获取', vcodeDisabled: false })
+            }
+        }, 1000)
+    }
+    post() {
+        let s = this.state
+        if (!!!s.newEmail || !$regex.isMail(s.newEmail)){ rb.highbar('请输入有效的邮箱地址'); return }
+        if (!!!s.newEmail || !!!s.vcode){ rb.highbar('请输入邮箱地址和验证码'); return }
+        let btns = $(this.refs['btns']).find('.btn').button('loading')
+        $.post(rb.baseUrl + '/settings/account/save-email?email=' + $encode(s.newEmail) + '&vcode=' + $encode(s.vcode), (res)=>{
+            btns.button('reset')
+            if (res.error_code == 0){
+                this.hide()
+                $('.J_email-account').html('当前绑定邮箱 <b>' + s.newEmail + '</b>')
+                rb.highbar('邮箱修改成功', 'success')
+            } else rb.highbar(res.error_msg)
+        })
+    }
 }
