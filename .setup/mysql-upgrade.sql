351255baa730b0949e3f77c31a9128586e39d9f9
devezhao <zhaofang123@gmail.com>
2019-03-03 20:44:24 +0800

    Update mysql-upgrade.sql

--- .setup/mysql-upgrade.sql
+++ .setup/mysql-upgrade.sql
@@ -15,3 +15,4 @@ create table if not exists `login_log` (
 )Engine=InnoDB;
 alter table `login_log`
   add index `IX1_login_log` (`USER`, `LOGIN_TIME`);
+INSERT INTO `layout_config` (`CONFIG_ID`, `BELONG_ENTITY`, `APPLY_TYPE`, `CONFIG`, `SHARE_TO`, `CREATED_ON`, `CREATED_BY`, `MODIFIED_ON`, `MODIFIED_BY`) VALUES ('013-016933622dc30002', 'LoginLog', 'DATALIST', '[{"field":"user"},{"field":"loginTime"},{"field":"userAgent"},{"field":"ipAddr"},{"field":"logoutTime"}]', 'ALL', '2019-02-28 17:14:26', '001-0000000000000001', '2019-02-28 17:37:36', '001-0000000000000001');
