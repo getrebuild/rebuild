4336e2a8833e8dabc9cda28d2353d8a540b7f0ed
devezhao <zhaofang123@gmail.com>
2019-06-29 18:14:34 +0800

    styles

--- src/main/webapp/assets/js/entity/approval-design.jsx
+++ src/main/webapp/assets/js/entity/approval-design.jsx
@@ -10,14 +10,15 @@ const NTs = {
 }
 
 const AddNodeButton = function (props) {
-  return (<div className="add-node-btn-box"><div className="add-node-btn"><button type="button"><i className="zmdi zmdi-plus" /></button></div></div>)
+  props = props || { addNode: function () { alert('添加节点') } }
+  return (<div className="add-node-btn-box"><div className="add-node-btn"><button type="button" onClick={props.addNode}><i className="zmdi zmdi-plus" /></button></div></div>)
 }
 
 // ~ 节点
 class Node extends React.Component {
   constructor(props) {
     super(props)
-    this.nodeType = NTs[props.type || 'start']
+    this.nodeType = NTs[props.type || 'approver']
   }
   render() {
     return (<div className="node-wrap">
@@ -30,7 +31,7 @@ class Node extends React.Component {
           </div>
         </div>
       </div>
-      <AddNodeButton />
+      <AddNodeButton addNode={showDlgAddNode} />
     </div>)
   }
 }
@@ -39,39 +40,59 @@ class Node extends React.Component {
 class ConditionNode extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { cols: props.cols }
+    this.state = { columns: props.columns || [] }
   }
   render() {
-    let colLength = (this.state.cols || []).length
+    let colLen = this.state.columns.length - 1
     return (<div className="branch-wrap">
       <div className="branch-box-wrap">
         <div className="branch-box">
-          <button className="add-branch">添加分支</button>
-          {(this.state.cols || []).map((col, idx) => {
-            return (<div className="col-box" key={'col-' + idx}>
-              {idx === 0 && <div className="top-left-cover-line"></div>}
-              {idx === 0 && <div className="bottom-left-cover-line"></div>}
-              <div className="condition-node">
-                <div className="condition-node-box">
-                  <div className="auto-judge">
-                    <div className="title-wrapper">
-                      <span className="editable-title float-left">条件1</span>
-                      <span className="priority-title float-right">优先级1</span>
-                    </div>
-                    <div className="content">
-                      请设置条件
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {idx === colLength - 1 && <div className="top-right-cover-line"></div>}
-              {idx === colLength - 1 && <div className="bottom-right-cover-line"></div>}
-            </div>)
+          <button className="add-branch" onClick={() => this.addColoum()}>添加分支</button>
+          {this.state.columns.map((item, idx) => {
+            return <ConditionColoum key={'column-' + idx} isFirst={idx === 0} isLast={idx === colLen} nodes={item.nodes} />
           })}
         </div>
         <AddNodeButton />
       </div>
     </div>)
+  }
+
+  addColoum() {
+    let columns = this.state.columns
+    columns.push('c')
+    this.setState({ columns: columns })
+  }
+}
+
+class ConditionColoum extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { ...props }
+  }
+  render() {
+    return (<div className="col-box">
+      {this.state.isFirst && <div className="top-left-cover-line"></div>}
+      {this.state.isFirst && <div className="bottom-left-cover-line"></div>}
+      <div className="condition-node">
+        <div className="condition-node-box">
+          <div className="auto-judge">
+            <div className="title-wrapper">
+              <span className="editable-title float-left">分支条件</span>
+              <span className="priority-title float-right">默认优先级</span>
+            </div>
+            <div className="content">
+              请设置条件
+            </div>
+          </div>
+        </div>
+      </div>
+      {this.state.nodes}
+      {this.state.isLast && <div className="top-right-cover-line"></div>}
+      {this.state.isLast && <div className="bottom-right-cover-line"></div>}
+    </div>)
+  }
+  componentWillReceiveProps(props) {
+    this.setState({ ...props })
   }
 }
 
@@ -85,11 +106,61 @@ class RbFlow extends React.Component {
       <Node type="start" />
       <Node type="approver" />
       <Node type="cc" />
-      <ConditionNode cols={['a', 'b', 'c']} />
+      <ConditionNode columns={['a', 'b', 'c']} />
       <div className="end-node">
         <div className="end-node-circle"></div>
         <div className="end-node-text">流程结束</div>
       </div>
     </div>)
   }
+}
+
+class DlgAddNode extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="modal add-nodes" tabIndex="-1" ref={(c) => this._dlg = c}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header pb-0">
+              <button className="close" type="button" onClick={() => this.hide()}><span className="zmdi zmdi-close" /></button>
+            </div>
+            <div className="modal-body">
+              <div className="row">
+                <div className="col-4">
+                  <button>审批人</button>
+                </div>
+                <div className="col-4">
+                  <button>抄送人</button>
+                </div>
+                <div className="col-4">
+                  <button>条件分支</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  componentDidMount() {
+    this.show()
+  }
+  hide() {
+    $(this._dlg).modal('hide')
+  }
+  show() {
+    $(this._dlg).modal({ show: true, keyboard: true })
+  }
+}
+
+let __DlgAddNode
+const showDlgAddNode = function () {
+  if (__DlgAddNode) __DlgAddNode.show()
+  else __DlgAddNode = renderRbcomp(<DlgAddNode />)
+}
+const hideDlgAddNode = function () {
+  if (__DlgAddNode) __DlgAddNode.hide()
 }
