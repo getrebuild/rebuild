963c1a8f3e189525552b8dc1ac8fa2542d553f43
devezhao <zhaofang123@gmail.com>
2019-09-26 14:11:40 +0800

    better code

--- src/main/java/com/rebuild/web/admin/entityhub/AutoFillinControll.java
+++ src/main/java/com/rebuild/web/admin/entityhub/AutoFillinControll.java
@@ -18,16 +18,11 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 package com.rebuild.web.admin.entityhub;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+import cn.devezhao.commons.web.ServletUtils;
+import cn.devezhao.persist4j.Entity;
+import cn.devezhao.persist4j.Field;
+import cn.devezhao.persist4j.Record;
+import cn.devezhao.persist4j.engine.ID;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -39,12 +34,14 @@ import com.rebuild.server.service.configuration.AutoFillinConfigService;
 import com.rebuild.utils.JSONUtils;
 import com.rebuild.web.BasePageControll;
 import com.rebuild.web.PortalsConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import cn.devezhao.commons.web.ServletUtils;
-import cn.devezhao.persist4j.Entity;
-import cn.devezhao.persist4j.Field;
-import cn.devezhao.persist4j.Record;
-import cn.devezhao.persist4j.engine.ID;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author devezhao zhaofang123@gmail.com
@@ -128,22 +125,18 @@ public class AutoFillinControll extends BasePageControll implements PortalsConfi
 		
 		JSONArray rules = new JSONArray();
 		for (Object[] o : array) {
-			String sf = (String) o[1];
-			String tf = (String) o[2];
-			if (!sourceEntity.containsField(sf)) {
-				LOG.warn("Unknow field '" + sf + "' in '" + sourceEntity.getName() + "'");
+			String sourceField = (String) o[1];
+			String targetField = (String) o[2];
+			if (!MetadataHelper.checkAndWarnField(sourceEntity, sourceField)
+					|| !MetadataHelper.checkAndWarnField(targetEntity, targetField)) {
 				continue;
 			}
-			if (!targetEntity.containsField(tf)) {
-				LOG.warn("Unknow field '" + tf + "' in '" + targetEntity.getName() + "'");
-				continue;
-			}
-			
+
 			JSON rule = JSONUtils.toJSONObject(
 					new String[] { "id", "sourceField", "sourceFieldLabel", "targetField", "targetFieldLabel", "extConfig" }, 
 					new Object[] { o[0], 
-							sf, EasyMeta.getLabel(sourceEntity.getField(sf)), 
-							tf, EasyMeta.getLabel(targetEntity.getField(tf)), 
+							sourceField, EasyMeta.getLabel(sourceEntity.getField(sourceField)),
+							targetField, EasyMeta.getLabel(targetEntity.getField(targetField)),
 							JSON.parse((String) o[3]) });
 			rules.add(rule);
 		}
