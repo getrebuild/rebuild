18cf6053dbe2541c37b77ddde6230590d3b41965
devezhao-mbp <zhaofang123@gmail.com>
2020-02-08 23:28:45 +0800

    testcase

--- src/main/java/com/rebuild/web/admin/robot/SendNotificationControll.java
+++ src/main/java/com/rebuild/web/admin/robot/SendNotificationControll.java
@@ -22,9 +22,7 @@ import com.rebuild.server.helper.SMSender;
 import com.rebuild.web.BaseControll;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
