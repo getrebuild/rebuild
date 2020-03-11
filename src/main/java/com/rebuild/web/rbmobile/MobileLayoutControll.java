19a22e6082a9e34fadf2ae9a82db1849657bd9bb
devezhao <zhaofang123@gmail.com>
2020-03-11 14:22:15 +0800

    feat: Layout for mobile

--- src/main/java/com/rebuild/web/rbmobile/MobileLayoutControll.java
+++ src/main/java/com/rebuild/web/rbmobile/MobileLayoutControll.java
@@ -7,18 +7,61 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 package com.rebuild.web.rbmobile;
 
+import cn.devezhao.persist4j.engine.ID;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.rebuild.server.configuration.portals.NavManager;
+import com.rebuild.utils.JSONUtils;
 import com.rebuild.web.BaseControll;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Iterator;
+
 /**
- * TODO 移动端布局
+ * 移动端布局
  *
  * @author devezhao
  * @since 2020/3/10
  */
 @Controller
-@RequestMapping("/mobile/user/")
+@RequestMapping("/mobile/layout/")
 @RbMobile
 public class MobileLayoutControll extends BaseControll {
+
+    @RequestMapping("navigation")
+    public void navigation(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 注意顺序
+        JSONArray topNav = new JSONArray();
+        topNav.add(buildNavItem("Home", "首页", null));
+        topNav.add(buildNavItem("Feeds", "动态", null));
+        JSONObject worksNav = buildNavItem("Works", "工作", null);
+        topNav.add(worksNav);
+        topNav.add(buildNavItem("Files", "文件", null));
+        topNav.add(buildNavItem("Me", "我的", null));
+
+        final ID user = getRequestUser(request);
+        JSONArray worksChild = NavManager.instance.getNavForPortal(user);
+
+        // 移除已有模块
+        for (Iterator<Object> iter = worksChild.iterator(); iter.hasNext(); ) {
+            String value = ((JSONObject) iter.next()).getString("value");
+            if (NavManager.NAV_FEEDS.equals(value) || NavManager.NAV_FILEMRG.equals(value)) {
+                iter.remove();
+            }
+        }
+        worksNav.put("child", worksChild);
+
+        writeSuccess(response, topNav);
+    }
+
+    private JSONObject buildNavItem(String value, String text, String icon) {
+        return JSONUtils.toJSONObject(
+                new String[] { "value", "text", "icon" },
+                new Object[] { value, text, icon });
+    }
+
 }
