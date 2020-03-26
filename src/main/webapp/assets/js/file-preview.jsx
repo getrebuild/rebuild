fdbd5381c3aba5f3d031a2173c766efa3cd0a39c
devezhao <zhaofang123@gmail.com>
2020-03-26 20:41:53 +0800

    fix: empty file

--- src/main/webapp/assets/js/file-preview.jsx
+++ src/main/webapp/assets/js/file-preview.jsx
@@ -94,7 +94,9 @@ class RbPreview extends React.Component {
   renderText() {
     return <div className="container fp-content">
       <div className="iframe text" onClick={this.__stopEvent}>
-        {this.state.previewText ? <pre>{this.state.previewText}</pre> : <div className="must-center"><RbSpinner fully={true} /></div>}
+        {(this.state.previewText || this.state.previewText === '')
+          ? <pre>{this.state.previewText || <i className="text-muted">无内容</i>}</pre>
+          : <div className="must-center"><RbSpinner fully={true} /></div>}
       </div>
     </div>
   }
