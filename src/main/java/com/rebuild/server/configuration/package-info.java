10e34077a082a1e1b71c84be1e656d76098b617a
devezhao-mbp <zhaofang123@gmail.com>
2020-04-28 01:54:51 +0800

    package-info

--- src/main/java/com/rebuild/server/configuration/package-info.java
+++ src/main/java/com/rebuild/server/configuration/package-info.java
@@ -6,6 +6,6 @@ See LICENSE and COMMERCIAL in the project root for license information.
 */
 
 /**
- * 主要是 Controll 控制器，连接前端（WEB）与后端，不涉及任何业务逻辑。此包可以被完整的安全的移除
+ * 系统配置管理
  */
-package com.rebuild.web;
+package com.rebuild.server.configuration;
