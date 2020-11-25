6390a5900c28ac41d545faefdef0f9d1726c4cdf
devezhao-mbp <zhaofang123@gmail.com>
2020-11-25 19:42:22 +0800

    norbv

--- src/main/java/com/rebuild/core/service/trigger/ActionFactory.java
+++ src/main/java/com/rebuild/core/service/trigger/ActionFactory.java
@@ -7,11 +7,27 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 package com.rebuild.core.service.trigger;
 
+import com.rebuild.core.service.general.OperatingContext;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author devezhao zhaofang123@gmail.com
  * @since 2019/05/24
  */
+@Slf4j
 public class ActionFactory {
+
+    private static final TriggerAction NORBV_FOUND = new TriggerAction() {
+        @Override
+        public ActionType getType() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void execute(OperatingContext operatingContext) throws TriggerException {
+            throw new UnsupportedOperationException();
+        }
+    };
 
     /**
      * @return
@@ -36,6 +52,12 @@ public class ActionFactory {
     public static TriggerAction createAction(String type, ActionContext context) {
         try {
             return ActionType.valueOf(type).newInstance(context);
+        } catch (ClassNotFoundException rbv) {
+            if (rbv.getLocalizedMessage().contains(".rbv.")) {
+                return NORBV_FOUND;
+            } else {
+                throw new TriggerException("Unknown trigger type : " + type, rbv);
+            }
         } catch (ReflectiveOperationException ex) {
             throw new TriggerException("Unknown trigger type : " + type, ex);
         }
