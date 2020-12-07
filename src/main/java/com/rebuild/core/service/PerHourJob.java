bed7894887bb295831f864026033ce839b1f7b8c
devezhao <zhaofang123@gmail.com>
2020-12-07 18:37:01 +0800

    checks

--- src/main/java/com/rebuild/core/service/PerHourJob.java
+++ src/main/java/com/rebuild/core/service/PerHourJob.java
@@ -7,21 +7,16 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 package com.rebuild.core.service;
 
-import com.alibaba.fastjson.JSONObject;
-import com.rebuild.core.Application;
-import com.rebuild.core.cache.CommonsCache;
+import com.rebuild.core.support.CheckDangers;
 import com.rebuild.core.support.ConfigurationItem;
-import com.rebuild.core.support.License;
 import com.rebuild.core.support.RebuildConfiguration;
 import com.rebuild.core.support.distributed.DistributedJobLock;
-import com.rebuild.core.support.i18n.Language;
 import com.rebuild.core.support.setup.DatabaseBackup;
 import com.rebuild.utils.FileFilterByLastModified;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
-import java.util.LinkedHashMap;
 
 /**
  * 每小时执行一次的 Job
@@ -45,7 +40,7 @@ public class PerHourJob extends DistributedJobLock {
         }
 
         if (hour % 2 == 0) {
-            doCheckAdminDangers();
+            new CheckDangers().checks();
         }
 
         // DO OTHERS HERE ...
@@ -68,31 +63,5 @@ public class PerHourJob extends DistributedJobLock {
      */
     protected void doCleanTempFiles() {
         FileFilterByLastModified.deletes(RebuildConfiguration.getFileOfTemp(null), 7);
-    }
-
-    public static final String CKEY_ADMIN_DANGERS = "ADMIN_DANGERS";
-
-    /**
-     * 管理员告警消息
-     */
-    @SuppressWarnings("unchecked")
-    protected void doCheckAdminDangers() {
-        LinkedHashMap<String, Object> dangers = (LinkedHashMap<String, Object>) Application.getCommonsCache().getx(CKEY_ADMIN_DANGERS);
-        if (dangers == null) {
-            dangers = new LinkedHashMap<>();
-        }
-
-        JSONObject checkBuild = License.siteApi("api/authority/check-build", true);
-        if (checkBuild != null && checkBuild.getIntValue("build") > Application.BUILD) {
-            String hasUpdate = Language.LF(
-                    "NewVersion", checkBuild.getString("version"), checkBuild.getString("releaseUrl"));
-            hasUpdate = hasUpdate.replace("<a ", "<a target='_blank' class='link' ");
-            dangers.put("hasUpdate", hasUpdate);
-        } else {
-            dangers.remove("hasUpdate");
-        }
-
-        // 放入缓存
-        Application.getCommonsCache().putx(CKEY_ADMIN_DANGERS, dangers, CommonsCache.TS_DAY * 2);
     }
 }
---
+++ src/main/java/com/rebuild/core/support/CheckDangers.java
@@ -0,0 +1,91 @@
+/*
+Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights reserved.
+
+rebuild is dual-licensed under commercial and open source licenses (GPLv3).
+See LICENSE and COMMERCIAL in the project root for license information.
+*/
+
+package com.rebuild.core.support;
+
+import com.alibaba.fastjson.JSONObject;
+import com.rebuild.core.Application;
+import com.rebuild.core.ServerStatus;
+import com.rebuild.core.cache.CommonsCache;
+import com.rebuild.core.support.i18n.Language;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+/**
+ * @author devezhao
+ * @since 2020/12/7
+ */
+@SuppressWarnings("unchecked")
+public class CheckDangers {
+
+    private static final String CKEY_DANGERS = "_DANGERS";
+
+    /**
+     */
+    public void checks() {
+        LinkedHashMap<String, String> dangers = (LinkedHashMap<String, String>) Application.getCommonsCache().getx(CKEY_DANGERS);
+        if (dangers == null) {
+            dangers = new LinkedHashMap<>();
+        }
+
+        JSONObject checkBuild = License.siteApi("api/authority/check-build", true);
+        if (checkBuild != null && checkBuild.getIntValue("build") > Application.BUILD) {
+            String hasUpdate = Language.LF(
+                    "NewVersion", checkBuild.getString("version"), checkBuild.getString("releaseUrl"));
+            hasUpdate = hasUpdate.replace("<a ", "<a target='_blank' class='link' ");
+            dangers.put("HasUpdate", hasUpdate);
+        } else {
+            dangers.remove("HasUpdate");
+        }
+
+        JSONObject echoValidity = License.siteApi("api/authority/echo?once=" + ServerStatus.STARTUP_ONCE, false);
+        if (echoValidity != null && !echoValidity.isEmpty()) {
+            String adminMsg = echoValidity.getString("adminMsg");
+            if (adminMsg == null) dangers.remove("AdminMsg");
+            else dangers.put("AdminMsg", adminMsg);
+
+            String usersMsg = echoValidity.getString("usersMsg");
+            if (usersMsg == null) dangers.remove("UsersMsg");
+            else dangers.put("UsersMsg", usersMsg);
+
+        } else {
+            dangers.remove("AdminMsg");
+            dangers.remove("UsersMsg");
+        }
+
+        // 放入缓存
+        Application.getCommonsCache().putx(CKEY_DANGERS, dangers, CommonsCache.TS_DAY * 2);
+    }
+
+    // --
+
+    /**
+     * @return
+     */
+    public static Collection<String> getAdminDangers() {
+        LinkedHashMap<String, String> dangers = (LinkedHashMap<String, String>) Application.getCommonsCache().getx(CKEY_DANGERS);
+        if (dangers == null || dangers.isEmpty()) {
+            return null;
+        }
+
+        dangers = (LinkedHashMap<String, String>) dangers.clone();
+        dangers.remove("UsersMsg");
+        return dangers.values();
+    }
+
+    /**
+     * @return
+     */
+    public static String getUserDanger() {
+        LinkedHashMap<String, String> dangers = (LinkedHashMap<String, String>) Application.getCommonsCache().getx(CKEY_DANGERS);
+        if (dangers == null || dangers.isEmpty()) {
+            return null;
+        }
+        return dangers.get("UsersMsg");
+    }
+}
