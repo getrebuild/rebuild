d90cd7d111bdc81a9b3f2af433935e61ab8c2eeb
devezhao <zhaofang123@gmail.com>
2020-12-07 13:32:30 +0800

    ver: 2.2

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -64,11 +64,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.1.0-beta1";
+    public static final String VER = "2.2.0-dev";
     /**
      * Rebuild Build
      */
-    public static final int BUILD = 20100;
+    public static final int BUILD = 20200;
 
     static {
         // Driver for DB
