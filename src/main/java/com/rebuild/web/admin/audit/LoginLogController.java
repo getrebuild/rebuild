bca6c2eb4cb12796a6a4b0f0def1da172aee366e
devezhao <zhaofang123@gmail.com>
2020-12-17 22:45:15 +0800

    better code

--- src/main/java/com/rebuild/web/admin/audit/LoginLogController.java
+++ src/main/java/com/rebuild/web/admin/audit/LoginLogController.java
@@ -12,6 +12,7 @@ import cn.devezhao.persist4j.engine.ID;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.rebuild.api.RespBody;
 import com.rebuild.core.Application;
 import com.rebuild.core.configuration.general.DataListManager;
 import com.rebuild.core.privileges.UserHelper;
@@ -20,13 +21,12 @@ import com.rebuild.utils.JSONUtils;
 import com.rebuild.utils.LocationUtils;
 import com.rebuild.web.EntityController;
 import com.rebuild.web.OnlineSessionStore;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
 
@@ -34,7 +34,7 @@ import java.util.Date;
  * @author devezhao-mac zhaofang123@gmail.com
  * @since 2019/02/16
  */
-@Controller
+@RestController
 public class LoginLogController extends EntityController {
 
     @GetMapping("/admin/audit/login-logs")
@@ -47,7 +47,7 @@ public class LoginLogController extends EntityController {
     }
 
     @GetMapping("/admin/audit/online-users")
-    public void getOnlineUsers(HttpServletResponse response) {
+    public JSON getOnlineUsers() {
         JSONArray users = new JSONArray();
         for (HttpSession s : Application.getSessionStore().getAllSession()) {
             ID user = (ID) s.getAttribute(WebUtils.CURRENT_USER);
@@ -55,23 +55,24 @@ public class LoginLogController extends EntityController {
 
             Object[] active = (Object[]) s.getAttribute(OnlineSessionStore.SK_LASTACTIVE);
             if (active == null) {
-                active = new Object[]{null, "/dashboard/home"};
+                active = new Object[]{ null, "/dashboard/home" };
             } else {
                 active = active.clone();
                 active[0] = I18nUtils.formatDate(new Date((Long) active[0]));
             }
 
             JSONObject item = JSONUtils.toJSONObject(
-                    new String[]{"user", "fullName", "activeTime", "activeUrl"},
-                    new Object[]{user, UserHelper.getName(user), active[0], active[1]});
+                    new String[] { "user", "fullName", "activeTime", "activeUrl" },
+                    new Object[] { user, UserHelper.getName(user), active[0], active[1] });
             users.add(item);
         }
-        writeSuccess(response, users);
+        return users;
     }
 
     @RequestMapping("/admin/audit/kill-session")
-    public void killSession(HttpServletRequest request, HttpServletResponse response) {
-        ID user = getIdParameterNotNull(request, "user");
+    public RespBody killSession(HttpServletRequest request) {
+        final ID user = getIdParameterNotNull(request, "user");
+
         HttpSession s = Application.getSessionStore().getSession(user);
         if (s != null) {
             LOG.warn("Kill session via admin : " + user + " < " + s.getId());
@@ -80,17 +81,18 @@ public class LoginLogController extends EntityController {
             } catch (Exception ignored) {
             }
         }
-        writeSuccess(response);
+        return RespBody.ok();
     }
 
     @GetMapping("/commons/ip-location")
-    public void getIpLocation(HttpServletRequest request, HttpServletResponse response) {
+    public RespBody getIpLocation(HttpServletRequest request) {
         String ip = getParameterNotNull(request, "ip");
         try {
             JSON location = LocationUtils.getLocation(ip);
-            writeSuccess(response, location);
+            return RespBody.ok(location);
+
         } catch (Exception ex) {
-            writeFailure(response);
+            return RespBody.error();
         }
     }
 }
