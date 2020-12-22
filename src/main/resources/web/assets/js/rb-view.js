ce8421e69820e8019f29dba08519818a5a8648b0
devezhao <zhaofang123@gmail.com>
2020-12-22 23:04:25 +0800

    fix Feeds in view

--- src/main/resources/web/assets/js/rb-view.js
+++ src/main/resources/web/assets/js/rb-view.js
@@ -339,7 +339,7 @@ class ReducedFeedsList extends FeedsList {
         {this.state.data && this.state.data.length === 0 && (
           <div className="list-nodata">
             <span className="zmdi zmdi-chart-donut" />
-            <p>{$L('NoData')}</p>
+            <p>{$L('NoSome,FeedsType2')}</p>
           </div>
         )}
         <div className="feeds-list inview">
@@ -376,9 +376,9 @@ class ReducedFeedsList extends FeedsList {
     const pageSize = 20
 
     $.post(`/feeds/feeds-list?pageNo=${this.__pageNo}&sort=&type=&foucs=&pageSize=${pageSize}`, JSON.stringify(filter), (res) => {
-      const _data = (res.data || {}).data || []
-      const _list = (this.state.data || []).concat(_data)
-      this.setState({ data: _list, showMores: _data.length >= pageSize })
+      const data = (res.data || {}).data || []
+      const list = (this.state.data || []).concat(data)
+      this.setState({ data: list, showMores: data.length >= pageSize })
     })
   }
 
