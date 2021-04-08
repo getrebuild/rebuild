e3a51e92f9715e72b6177180fcbb2b678b220b09
devezhao <zhaofang123@gmail.com>
2021-04-08 16:54:53 +0800

    Add .deploy files

---
+++ .deploy/restart-rebuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JARFILE=rebuild-boot.jar
+
+echo Use jar file [ $JARFILE ]
+
+PID=$(ps -ef | grep $JARFILE | grep -v grep | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+  echo Rebuild-Boot is already stopped
+else
+  echo Stopping Rebuild-Boot \(kill $PID\) ...
+  kill $PID
+  sleep 10
+fi
+
+echo "Starting Rebuild-Boot ..."
+nohup java -Xms1001M -Xmx1001M -XX:+UseG1GC -jar $JARFILE >/dev/null 2>&1 &
+sleep 5
+
+echo done
+exit 0
