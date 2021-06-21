25eff0d4dc8c09db96d275c40d9bf10924bbd900
devezhao <>
2021-06-21 22:37:37 +0800

    2.5.0-dev

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -62,11 +62,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.4.0";
+    public static final String VER = "2.5.0-dev";
     /**
      * Rebuild Build
      */
-    public static final int BUILD = 20400;
+    public static final int BUILD = 20500;
 
     static {
         // Driver for DB
