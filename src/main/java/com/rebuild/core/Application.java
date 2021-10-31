4c4a36df56be76e5a3e26fcfb5963d8d1afa44f9
RB <42044143+getrebuild@users.noreply.github.com>
2021-10-31 23:22:02 +0800

    Fix 2.6.0 (#397)
    * flag v2.6.0
    
    * fix: upload type
    
    * fix list-mode2
    
    * 2.6.0-beta1
    
    Co-authored-by: devezhao <>

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -63,11 +63,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.6.0-beta";
+    public static final String VER = "2.6.0-beta1";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2060000;
+    public static final int BUILD = 2060001;
 
     static {
         // Driver for DB
