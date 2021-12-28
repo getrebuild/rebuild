1ccefbb308194ebe77256bd2c991a6fbc0a790ee
devezhao <>
2021-12-28 13:16:52 +0800

    2.8-dev

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -64,11 +64,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.7.0-beta1";
+    public static final String VER = "2.8.0-dev";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2070001;
+    public static final int BUILD = 2080001;
 
     static {
         // Driver for DB
