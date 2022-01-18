7b59e0412f56f61bdb4ec914999bf8637deea678
devezhao <zhaofang123@gmail.com>
2022-01-18 17:03:55 +0800

    ci

--- src/main/java/com/rebuild/utils/HttpUtils.java
+++ src/main/java/com/rebuild/utils/HttpUtils.java
@@ -7,6 +7,7 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 package com.rebuild.utils;
 
+import cn.devezhao.commons.identifier.ComputerIdentifier;
 import com.alibaba.fastjson.JSON;
 import com.rebuild.core.Application;
 import com.rebuild.core.support.RebuildConfiguration;
@@ -36,6 +37,7 @@ public class HttpUtils {
     private static final String RB_UA =
             String.format("RB/%s (%s/%s)", Application.VER, SystemUtils.OS_NAME, SystemUtils.JAVA_SPECIFICATION_VERSION);
     private static final String RB_LANG = Locale.getDefault().toString();  // zh_CN
+    private static String RB_CI;
 
     /**
      * 获取客户端，如本类提供的 GET/POST 方法无法满足需求，可以自己构建然后通过此 Client 调用。
@@ -51,6 +53,7 @@ public class HttpUtils {
                     .readTimeout(30, TimeUnit.SECONDS)
                     .retryOnConnectionFailure(true)
                     .build();
+            RB_CI = ComputerIdentifier.generateIdentifierKey();
         }
         return okHttpClient;
     }
@@ -75,11 +78,12 @@ public class HttpUtils {
      * @throws IOException
      */
     public static String get(String url, Map<String, String> headers) throws IOException {
+        OkHttpClient client = getHttpClient();
         Request.Builder builder = new Request.Builder().url(url);
         Request request = useHeaders(builder, headers).build();
 
         long ms = System.currentTimeMillis();
-        try (Response response = getHttpClient().newCall(request).execute()) {
+        try (Response response = client.newCall(request).execute()) {
             return Objects.requireNonNull(response.body()).string();
         } finally {
             ms = System.currentTimeMillis() - ms;
@@ -129,13 +133,14 @@ public class HttpUtils {
             requestBody = RequestBody.create(reqData == null ? "" : reqData.toString(), MediaType.parse("text/plain"));
         }
 
+        OkHttpClient client = getHttpClient();
         Request.Builder builder = new Request.Builder().url(url);
         Request request = useHeaders(builder, headers)
                 .post(requestBody)
                 .build();
 
         long ms = System.currentTimeMillis();
-        try (Response response = getHttpClient().newCall(request).execute()) {
+        try (Response response = client.newCall(request).execute()) {
             return Objects.requireNonNull(response.body()).string();
         } finally {
             ms = System.currentTimeMillis() - ms;
@@ -166,10 +171,11 @@ public class HttpUtils {
      * @throws IOException
      */
     public static boolean readBinary(String url, File dest, Map<String, String> headers) throws IOException {
+        OkHttpClient client = getHttpClient();
         Request.Builder builder = new Request.Builder().url(url);
         Request request = useHeaders(builder, headers).build();
 
-        try (Response response = getHttpClient().newCall(request).execute()) {
+        try (Response response = client.newCall(request).execute()) {
             try (InputStream is = Objects.requireNonNull(response.body()).byteStream()) {
                 try (BufferedInputStream bis = new BufferedInputStream(is)) {
                     try (OutputStream os = new FileOutputStream(dest)) {
@@ -189,6 +195,7 @@ public class HttpUtils {
     private static Request.Builder useHeaders(Request.Builder builder, Map<String, String> headers) {
         builder.addHeader(HttpHeaders.USER_AGENT, RB_UA);
         builder.addHeader(HttpHeaders.ACCEPT_LANGUAGE, RB_LANG);
+        if (RB_CI != null) builder.addHeader("X-RB-CI", RB_CI);
 
         if (headers != null && !headers.isEmpty()) {
             for (Map.Entry<String, String> e : headers.entrySet()) {
