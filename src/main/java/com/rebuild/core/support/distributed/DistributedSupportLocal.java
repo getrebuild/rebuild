a12193e6c720af1d8bb79c77c5193560986b53d8
devezhao <zhaofang123@gmail.com>
2022-02-17 12:33:48 +0800

    Copyright

--- src/main/java/com/rebuild/core/support/distributed/DistributedSupportLocal.java
+++ src/main/java/com/rebuild/core/support/distributed/DistributedSupportLocal.java
@@ -1,5 +1,8 @@
 /*
-Copyright (c) Ruifang Tech <http://ruifang-tech.com/> and/or its owners. All rights reserved.
+Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights reserved.
+
+rebuild is dual-licensed under commercial and open source licenses (GPLv3).
+See LICENSE and COMMERCIAL in the project root for license information.
 */
 
 package com.rebuild.core.support.distributed;
