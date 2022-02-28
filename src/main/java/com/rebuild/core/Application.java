692e732fbdc40ce392d77feedf3523426bbf8196
Zixin <42044143+getrebuild@users.noreply.github.com>
2022-02-28 22:18:23 +0800

    beta

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -64,7 +64,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.8.0-dev";
+    public static final String VER = "2.8.0-beta";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
