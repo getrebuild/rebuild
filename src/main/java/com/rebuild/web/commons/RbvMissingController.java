0b2e32b88f9471572b7da1658fccd5824056d514
RB <getrebuild@sina.com>
2022-03-16 15:28:15 +0800

    fix: rbv tips

--- src/main/java/com/rebuild/web/commons/RbvMissingController.java
+++ src/main/java/com/rebuild/web/commons/RbvMissingController.java
@@ -15,7 +15,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 /**
+ * When none RBV
+ *
  * @author ZHAO
  * @since 2021/6/21
  */
@@ -24,9 +29,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class RbvMissingController extends BaseController {
 
     @GetMapping({"/h5app/**"})
-    public ModelAndView pageH5app() {
+    public ModelAndView h5app() {
         ModelAndView mv = ErrorPageView.createErrorPage(
                 Language.L("免费版不支持手机访问功能 [(查看详情)](https://getrebuild.com/docs/rbv-features)"));
+        mv.getModelMap().put(WebConstants.$BUNDLE, Language.getCurrentBundle());
+        return mv;
+    }
+
+    @GetMapping("/user/login/sso")
+    public ModelAndView ssoLogin(HttpServletRequest request) throws IOException {
+        String error = "dingtalk".equalsIgnoreCase(getParameterNotNull(request, "protocol"))
+                ? Language.L("免费版不支持钉钉集成 [(查看详情)](https://getrebuild.com/docs/rbv-features)")
+                : Language.L("免费版不支持企业微信集成 [(查看详情)](https://getrebuild.com/docs/rbv-features)");
+
+        ModelAndView mv = ErrorPageView.createErrorPage(error);
         mv.getModelMap().put(WebConstants.$BUNDLE, Language.getCurrentBundle());
         return mv;
     }
