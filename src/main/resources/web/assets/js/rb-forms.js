99620e64e8d5b923b0d417171a907b38a8949a7c
RB <getrebuild@sina.com>
2022-04-06 21:53:58 +0800

    fix: calcFormula

--- src/main/resources/web/assets/js/rb-forms.js
+++ src/main/resources/web/assets/js/rb-forms.js
@@ -347,9 +347,9 @@ class RbForm extends React.Component {
   }
 
   // 避免无意义更新
-  setFieldUnchanged(field) {
+  setFieldUnchanged(field, originValue) {
     delete this.__FormData[field]
-    if (this._onFieldValueChange_calls) this._onFieldValueChange_calls.forEach((c) => c({ name: field }))
+    if (this._onFieldValueChange_calls) this._onFieldValueChange_calls.forEach((c) => c({ name: field, value: originValue }))
 
     // eslint-disable-next-line no-console
     if (rb.env === 'dev') console.log('FV2 ... ' + JSON.stringify(this.__FormData))
@@ -574,7 +574,7 @@ class RbFormElement extends React.Component {
 
     if (this.isValueUnchanged() && !this.props.$$$parent.isNew) {
       if (err) this.props.$$$parent.setFieldValue(this.props.field, this.state.value, errMsg)
-      else this.props.$$$parent.setFieldUnchanged(this.props.field)
+      else this.props.$$$parent.setFieldUnchanged(this.props.field, this.state.value)
     } else {
       this.setState({ hasError: err })
       this.props.$$$parent.setFieldValue(this.props.field, this.state.value, errMsg)
@@ -645,8 +645,8 @@ class RbFormElement extends React.Component {
   }
 
   // Setter
-  setValue(val, checkValue) {
-    this.handleChange({ target: { value: val } }, checkValue === false ? false : true)
+  setValue(val) {
+    this.handleChange({ target: { value: val } }, true)
   }
   // Getter
   // @return string or object
@@ -796,7 +796,7 @@ class RbFormNumber extends RbFormText {
       // 表单计算
       this.props.$$$parent.onFieldValueChange((s) => {
         if (!watchFields.includes(`{${s.name}}`)) return
-        debugger
+
         if (s.value) {
           this.calcFormula__values[s.name] = this._removeComma(s.value)
         } else {
@@ -809,7 +809,7 @@ class RbFormNumber extends RbFormText {
         }
 
         if (formula.includes('{')) {
-          this.setValue(null, false)
+          this.setValue(null)
           return
         }
 
