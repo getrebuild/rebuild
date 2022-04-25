39ab712324e02b212193075cc55cd28332f58b3f
RB <42044143+getrebuild@users.noreply.github.com>
2022-04-25 13:44:17 +0800

    Fix 2.8.4 (#459)
    * fix: rule for GroupAggregation
    
    * v2.8.4

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -64,11 +64,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.8.3";
+    public static final String VER = "2.8.4";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2080308;
+    public static final int BUILD = 2080408;
 
     static {
         // Driver for DB
