0a8232be1295323d050038cab5b0b9a7851b6982
RB <getrebuild@sina.com>
2022-05-04 13:49:36 +0800

    lang

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -65,11 +65,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.9.0-dev";
+    public static final String VER = "2.9.0-beta1";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2090000;
+    public static final int BUILD = 2090001;
 
     static {
         // Driver for DB
