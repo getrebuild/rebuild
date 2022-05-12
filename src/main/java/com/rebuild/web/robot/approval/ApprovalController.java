808c48605cf6742e5104ef154cfd5b11290501b5
RB <getrebuild@sina.com>
2022-05-12 19:51:26 +0800

    sort reports, approvals

--- src/main/java/com/rebuild/web/robot/approval/ApprovalController.java
+++ src/main/java/com/rebuild/web/robot/approval/ApprovalController.java
@@ -45,11 +45,18 @@ public class ApprovalController extends BaseController {
         final ID user = getRequestUser(request);
 
         FlowDefinition[] defs = RobotApprovalManager.instance.getFlowDefinitions(recordId, user);
-        JSONArray data = new JSONArray();
+        JSONArray res = new JSONArray();
         for (FlowDefinition d : defs) {
-            data.add(d.toJSON("id", "name"));
+            res.add(d.toJSON("id", "name"));
         }
-        return data;
+
+        // 名称排序
+        res.sort((o1, o2) -> {
+            JSONObject j1 = (JSONObject) o1;
+            JSONObject j2 = (JSONObject) o2;
+            return j1.getString("name").compareTo(j2.getString("name"));
+        });
+        return res;
     }
 
     @GetMapping("state")
