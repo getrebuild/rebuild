1e0bec90574b41e85bc0caeb933b8cfa52493d64
RB <getrebuild@sina.com>
2022-05-16 13:58:32 +0800

    [better] date func

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -65,11 +65,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.9.0-beta2";
+    public static final String VER = "2.9.0-beta3";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2090002;
+    public static final int BUILD = 2090003;
 
     static {
         // Driver for DB
