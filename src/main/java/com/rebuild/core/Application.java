bcd11a41136b9e06ddd69d1738c77386a05b3ffc
RB <getrebuild@sina.com>
2022-05-27 16:50:06 +0800

    fix cve

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -65,11 +65,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "2.9.0";
+    public static final String VER = "2.9.1";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2090004;
+    public static final int BUILD = 2090105;
 
     static {
         // Driver for DB
