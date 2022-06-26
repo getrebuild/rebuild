4b51d4150bcd0dc7811b90c67ce122aa778e7aaf
RB <getrebuild@sina.com>
2022-06-26 18:12:27 +0800

    fix: rbv

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -69,7 +69,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 2090208;
+    public static final int BUILD = 2090209;
 
     static {
         // Driver for DB
