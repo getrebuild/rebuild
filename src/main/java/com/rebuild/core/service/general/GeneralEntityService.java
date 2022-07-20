37ae2b3249a83d9898b1fbe2061a7f56d347f4ea
RB <getrebuild@sina.com>
2022-07-20 14:52:54 +0800

    fix detail add

--- src/main/java/com/rebuild/core/service/general/GeneralEntityService.java
+++ src/main/java/com/rebuild/core/service/general/GeneralEntityService.java
@@ -105,12 +105,9 @@ public class GeneralEntityService extends ObservableService implements EntitySer
         boolean checkDetailsRepeated = rcm == GeneralEntityServiceContextHolder.RCM_CHECK_DETAILS
                 || rcm == GeneralEntityServiceContextHolder.RCM_CHECK_ALL;
 
-        // 使用明细实体 Service
-        final EntityService des = Application.getEntityService(record.getEntity().getDetailEntity().getEntityCode());
-
         // 先删除
         for (Record d : details) {
-            if (d instanceof DeleteRecord) des.delete(d.getPrimary());
+            if (d instanceof DeleteRecord) delete(d.getPrimary());
         }
 
         // 再保存
@@ -120,7 +117,7 @@ public class GeneralEntityService extends ObservableService implements EntitySer
             if (checkDetailsRepeated) {
                 d.setID(dtf, mainid);  // for check
 
-                List<Record> repeated = des.getAndCheckRepeated(d, 20);
+                List<Record> repeated = getAndCheckRepeated(d, 20);
                 if (!repeated.isEmpty()) {
                     throw new RepeatedRecordsException(repeated);
                 }
@@ -128,9 +125,9 @@ public class GeneralEntityService extends ObservableService implements EntitySer
 
             if (d.getPrimary() == null) {
                 d.setID(dtf, mainid);
-                des.create(d);
+                create(d);
             } else {
-                des.update(d);
+                update(d);
             }
         }
 
