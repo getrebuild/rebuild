ecf955645c7ef0b7e1cc18fb42fe458cbf5ebd26
Zixin <42044143+getrebuild@users.noreply.github.com>
2022-08-09 00:46:03 +0800

    make ver

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -65,11 +65,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "3.0.0-dev";
+    public static final String VER = "3.0.0-beta1";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{1}
      */
-    public static final int BUILD = 300000;
+    public static final int BUILD = 300001;
 
     static {
         // Driver for DB
