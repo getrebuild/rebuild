070dbd692257ac2bb8530ad985a2305ab22bb509
RB <getrebuild@sina.com>
2022-11-06 21:57:21 +0800

    fix: redis flushDB

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -71,7 +71,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 3010006;
+    public static final int BUILD = 3010007;
 
     static {
         // Driver for DB
