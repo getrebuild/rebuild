d7cfcef4fcaa7656af0a1ecaddd1c15bce5c1127
RB <getrebuild@sina.com>
2022-12-12 17:18:30 +0800

    tag init

---
+++ src/main/java/com/rebuild/core/metadata/easymeta/EasyTag.java
@@ -0,0 +1,34 @@
+/*!
+Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights reserved.
+
+rebuild is dual-licensed under commercial and open source licenses (GPLv3).
+See LICENSE and COMMERCIAL in the project root for license information.
+*/
+
+package com.rebuild.core.metadata.easymeta;
+
+import cn.devezhao.persist4j.Field;
+import com.rebuild.core.support.general.N2NReferenceSupport;
+
+/**
+ * @author Zixin
+ * @since 2022/12/12
+ * @see N2NReferenceSupport
+ */
+public class EasyTag extends EasyField {
+    private static final long serialVersionUID = -5827184319679918289L;
+
+    protected EasyTag(Field field, DisplayType displayType) {
+        super(field, displayType);
+    }
+
+    @Override
+    public Object convertCompatibleValue(Object value, EasyField targetField) {
+        return super.convertCompatibleValue(value, targetField);
+    }
+
+    @Override
+    public Object exprDefaultValue() {
+        return super.exprDefaultValue();
+    }
+}
