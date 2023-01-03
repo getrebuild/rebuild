77d1b1bb2dc356b134a1d1ed79723af38cea8921
RB <getrebuild@sina.com>
2023-01-03 17:19:51 +0800

    quick-search trim

--- src/main/resources/web/assets/js/rb-datalist.common.js
+++ src/main/resources/web/assets/js/rb-datalist.common.js
@@ -1084,7 +1084,7 @@ class RbList extends React.Component {
 
   _buildQuick(el) {
     el = $(el || '.input-search>input')
-    const q = el.val()
+    const q = $.trim(el.val())
     if (!q && !this.lastFilter) return null
 
     return {
