b44fad98297beed641afb611d68c20aa16d8a69d
RB <getrebuild@sina.com>
2023-01-17 21:15:42 +0800

    maven-compiler-plugin

--- src/test/java/com/rebuild/api/user/AuthTokenManagerTest.java
+++ src/test/java/com/rebuild/api/user/AuthTokenManagerTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
  */
 public class AuthTokenManagerTest extends TestSupport {
 
+    @SuppressWarnings("deprecation")
     @Test
     void accessToken() {
         // Generate
