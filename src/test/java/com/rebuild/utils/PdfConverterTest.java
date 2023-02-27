199dce15d046daef333b7c01de6827d011128d77
RB <getrebuild@sina.com>
2023-02-27 20:09:07 +0800

    Copyright

--- src/test/java/com/rebuild/utils/PdfConverterTest.java
+++ src/test/java/com/rebuild/utils/PdfConverterTest.java
@@ -1,3 +1,10 @@
+/*!
+Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights reserved.
+
+rebuild is dual-licensed under commercial and open source licenses (GPLv3).
+See LICENSE and COMMERCIAL in the project root for license information.
+*/
+
 package com.rebuild.utils;
 
 import org.junit.jupiter.api.Test;
@@ -5,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/**
+ */
 class PdfConverterTest {
 
     @Test
