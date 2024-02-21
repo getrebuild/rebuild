4ff69cdd8f08f2ae3953c617bce6fb2510e2d3cf
RB <getrebuild@sina.com>
2024-02-21 22:58:51 +0800

    be: TestCases

--- src/test/java/com/rebuild/utils/ExcelUtilsTest.java
+++ src/test/java/com/rebuild/utils/ExcelUtilsTest.java
@@ -8,17 +8,9 @@ See LICENSE and COMMERCIAL in the project root for license information.
 package com.rebuild.utils;
 
 import cn.devezhao.commons.excel.IRow;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFFont;
-import org.apache.poi.xssf.usermodel.XSSFRichTextString;
-import org.apache.poi.xssf.usermodel.XSSFSimpleShape;
-import org.apache.poi.xssf.usermodel.XSSFTextRun;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -35,26 +27,6 @@ class ExcelUtilsTest {
             System.out.println(row.getCell("b"));
             System.out.println(row.getCell("G"));
             System.out.println(row.getCell("h"));
-        }
-    }
-
-    @Test
-    void testShape() throws IOException {
-        try (Workbook wb = WorkbookFactory.create(new File("/Users/zhaoff/Desktop/1.xlsx"))) {
-            Sheet sheet = wb.getSheetAt(0);
-            for (Object o : sheet.getDrawingPatriarch()) {
-                XSSFSimpleShape shape = (XSSFSimpleShape) o;
-                XSSFTextRun s = shape.getTextParagraphs().get(0).getTextRuns().get(0);
-
-                XSSFFont font = (XSSFFont) wb.createFont();
-                font.setFontName(s.getFontFamily());
-                font.setFontHeightInPoints((short) s.getFontSize());
-                font.setBold(s.isBold());
-                font.setItalic(s.isItalic());
-
-                XSSFRichTextString richTextString = new XSSFRichTextString("11");
-                richTextString.applyFont(font);
-            }
         }
     }
 }
