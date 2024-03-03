e6bc4e9fe8ec2237f06b1b6f6db22d52f603de52
RB <getrebuild@sina.com>
2024-03-03 17:08:27 +0800

    Update file-preview.js

--- src/main/resources/web/assets/js/file-preview.js
+++ src/main/resources/web/assets/js/file-preview.js
@@ -7,9 +7,9 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 // ~~ 图片/文档预览
 
+const TYPE_TEXTS = ['.txt', '.xml', '.json', '.md', '.yml', '.css', '.js', '.htm', '.html', '.log', '.sql', '.conf', '.sh', '.bat']
 const TYPE_DOCS = ['.doc', '.docx', '.rtf', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf']
 const TYPE_IMGS = ['.jpg', '.jpeg', '.gif', '.png', '.bmp', '.jfif', '.svg', '.webp']
-const TYPE_TEXTS = ['.txt', '.xml', '.json', '.md', '.yml', '.css', '.js', '.htm', '.html', '.log', '.sql', '.conf', '.sh', '.bat']
 const TYPE_AUDIOS = ['.mp3', '.wma', '.m4a', '.flac', '.ogg', '.acc']
 const TYPE_VIDEOS = ['.mp4', '.wmv', '.mov', '.avi', '.mkv', '.webm', '.m4v', '.mpg', '.mpge']
 
