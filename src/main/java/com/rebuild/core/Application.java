ea86c421a9e67c69272a6c2659e1e68a0956c40f
REBUILD 企业管理系统 <42044143+getrebuild@users.noreply.github.com>
2024-03-11 21:00:18 +0800

    Fix 3.6 beta3 (#727)
    * 3.6-beta3
    
    * fix: Gitee#I971N4
    
    * Update url-safe.html

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -74,7 +74,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "3.6.0-beta2";
+    public static final String VER = "3.6.0-beta3";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
