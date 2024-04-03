639076f284a3713989e9d2299297cb617ca7b608
devezhao <zhaofang123@gmail.com>
2024-04-04 00:37:05 +0800

    be

--- src/main/resources/web/assets/js/charts/chart-design.js
+++ src/main/resources/web/assets/js/charts/chart-design.js
@@ -412,7 +412,7 @@ const render_preview = (_color) => {
 
       if (!(conf.type === 'CNMAP' || conf.type === 'DATALIST2')) {
         if ($('.J_axis-dim span[data-type="map"]')[0]) {
-          render_preview_error($L('位置字段仅适用于“地图”图表'))
+          render_preview_error($L('位置字段仅适用于“地图”、“数据列表”图表'))
           return
         }
       }
