2ab6fd9b7327b0f688e612e5f97698631250d77e
RB <getrebuild@sina.com>
2024-04-25 12:02:56 +0800

    hotfix: ALWAYS_PARSE_INTEGRAL_NUMBER_INTO_DECIMAL

--- src/main/java/com/rebuild/core/service/trigger/impl/FieldWriteback.java
+++ src/main/java/com/rebuild/core/service/trigger/impl/FieldWriteback.java
@@ -47,6 +47,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 
+import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -456,7 +457,7 @@ public class FieldWriteback extends FieldAggregation {
                             // 数字字段置 `0`
                             if (varField != null
                                     && (varField.getType() == FieldType.LONG || varField.getType() == FieldType.DECIMAL)) {
-                                value = 0;
+                                value = 0L;
                             } else if (fieldVarsN2NPath.contains(fieldName)
                                     || (isN2NField != null && isN2NField.getType() == FieldType.REFERENCE_LIST)) {
                                 // Keep NULL
@@ -476,6 +477,10 @@ public class FieldWriteback extends FieldAggregation {
                         } else if (value instanceof ID || forceUseQuote) {
                             value = value.toString();
                         }
+
+                        // v3.6.3 强制使用 BigDecimal 高精度
+                        if (value instanceof Long) value = BigDecimal.valueOf((Long) value);
+                        else if (value instanceof Integer) value = BigDecimal.valueOf((Integer) value);
 
                         envMap.put(fieldName, value);
                     }
