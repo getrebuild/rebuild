857e94aee0d3f0eea9b6a616808232d77987bf56
RB <getrebuild@sina.com>
2024-06-26 18:35:58 +0800

    feat: convertPictureWithSize

--- src/main/java/com/rebuild/core/service/datareport/ValueConvertFunc.java
+++ src/main/java/com/rebuild/core/service/datareport/ValueConvertFunc.java
@@ -9,13 +9,21 @@ package com.rebuild.core.service.datareport;
 
 import cn.devezhao.commons.CalendarUtils;
 import cn.hutool.core.convert.Convert;
+import com.deepoove.poi.data.PictureRenderData;
+import com.deepoove.poi.data.Pictures;
 import com.rebuild.core.metadata.easymeta.DisplayType;
 import com.rebuild.core.metadata.easymeta.EasyDecimal;
 import com.rebuild.core.metadata.easymeta.EasyField;
 import com.rebuild.utils.CommonsUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.Date;
 
@@ -70,6 +78,49 @@ public class ValueConvertFunc {
 
         }
         return value;
+    }
+
+    /**
+     * 转换图片
+     *
+     * @param value
+     * @param varName
+     * @return
+     */
+    public static PictureRenderData convertPictureWithSize(byte[] value, String varName) {
+        Pictures.PictureBuilder builder = Pictures.ofBytes(value);
+
+        String thatFunc = splitFunc(varName);
+        if (thatFunc == null) return builder.create();
+
+        if (thatFunc.startsWith("SIZE") && thatFunc.length() > 4) {
+            String[] wh = thatFunc.substring(4).split("\\*");
+            int width = NumberUtils.toInt(wh[0]);
+            int height = -1;
+
+            // 指定宽度，高度自适应
+            if (wh.length == 1) {
+                try (InputStream is = new ByteArrayInputStream(value)) {
+                    BufferedImage bi = ImageIO.read(is);
+                    int originWidth = bi.getWidth();
+                    int originHeight = bi.getHeight();
+                    if (originWidth > 0 && originHeight > 0) {
+                        double scale = width * 1.0 / originWidth;
+                        height = (int) (originHeight * scale);
+                    }
+
+                } catch (IOException e) {
+                    log.error(null, e);
+                }
+            } else {
+                height = NumberUtils.toInt(wh[1]);
+            }
+
+            if (height < 0) height = width;
+            builder = Pictures.ofBytes(value).size(width, height);
+        }
+
+        return builder.create();
     }
 
     /**
