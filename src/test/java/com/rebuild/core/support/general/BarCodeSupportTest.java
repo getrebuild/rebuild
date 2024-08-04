d2764f78ba8f28f76de2e48a881a9a5e00f0009b
RB <getrebuild@sina.com>
2024-08-04 15:12:39 +0800

    barcode decode

--- src/test/java/com/rebuild/core/support/general/BarCodeSupportTest.java
+++ src/test/java/com/rebuild/core/support/general/BarCodeSupportTest.java
@@ -12,12 +12,15 @@ import com.google.zxing.BarcodeFormat;
 import com.rebuild.TestSupport;
 import com.rebuild.core.metadata.MetadataHelper;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
 
 /**
  * @author devezhao
  * @since 2020/6/5
  */
-public class BarCodeGeneratorTest extends TestSupport {
+public class BarCodeSupportTest extends TestSupport {
 
     @Test
     void getBarCodeContent() {
@@ -29,5 +32,14 @@ public class BarCodeGeneratorTest extends TestSupport {
     void saveBarCode() {
         System.out.println(BarCodeSupport.saveCode(BarCodeSupport.createBarCodeImage("123ABC支持中文", BarcodeFormat.QR_CODE, 0, 0)));
         System.out.println(BarCodeSupport.saveCode(BarCodeSupport.createBarCodeImage("CODE128", BarcodeFormat.CODE_128, 0, 0)));
+    }
+
+    @Test
+    void decode() throws Exception {
+        File qrcode = ResourceUtils.getFile("classpath:qrcode_getrebuild.com.png").getAbsoluteFile();
+        System.out.println(BarCodeSupport.decode(qrcode));
+
+        File barcode = ResourceUtils.getFile("classpath:barcode.png").getAbsoluteFile();
+        System.out.println(BarCodeSupport.decode(barcode));
     }
 }
