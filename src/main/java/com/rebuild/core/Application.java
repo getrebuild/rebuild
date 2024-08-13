82e51fe46cd645dd27eb5066c61f6b6a262cacd7
REBUILD 企业管理系统 <42044143+getrebuild@users.noreply.github.com>
2024-08-13 19:20:34 +0800

    Fix 3.7.7 (#797)
    * 3.7.7
    
    * fix: Excel列表无法导出审批相关字段
    
    * Update @rbv

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -74,11 +74,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "3.7.6";
+    public static final String VER = "3.7.7";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 3070612;
+    public static final int BUILD = 3070713;
 
     static {
         // Driver for DB
