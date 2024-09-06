52dec00da50154d52197e57d3799a3516047c40f
RB <getrebuild@sina.com>
2024-09-06 23:31:06 +0800

    be

--- src/main/java/com/rebuild/core/support/CommandArgs.java
+++ src/main/java/com/rebuild/core/support/CommandArgs.java
@@ -33,6 +33,7 @@ public class CommandArgs {
     public static final String _UseFrontJSAnywhere = "_UseFrontJSAnywhere";
     public static final String _TriggerMaxDepth = "_TriggerMaxDepth";
     public static final String _ProtectedAdmin = "_ProtectedAdmin";
+    public static final String _TriggerLessLog = "_TriggerLessLog";
 
     /**
      * @param name
