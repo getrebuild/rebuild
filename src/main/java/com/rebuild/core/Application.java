101b89b043cb422b581c1848268402fff2712480
REBUILD 企业管理系统 <42044143+getrebuild@users.noreply.github.com>
2024-10-09 11:37:09 +0800

    Fix 3.8.1 (#818)
    * fix!!!
    
    * 3.8.1

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -74,11 +74,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "3.8.0";
+    public static final String VER = "3.8.1";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 3080004;
+    public static final int BUILD = 3080106;
 
     static {
         // Driver for DB
