04fc15f1c09a4510632245783bbda9a36a3881e7
RB <getrebuild@sina.com>
2025-01-09 16:14:52 +0800

    hotfix: task_relatedRecord

--- src/main/resources/web/assets/js/rb-components.js
+++ src/main/resources/web/assets/js/rb-components.js
@@ -972,13 +972,15 @@ class AnyRecordSelector extends RecordSelector {
             allowClear: false,
           })
           .on('change', (e) => {
-            this.setState(
-              {
-                entity: e.target.value,
-                entityLabel: this.__select2Entity.select2('data')[0].text,
-              },
-              () => this._initSelect2(true)
-            )
+            if (e.target.value) {
+              this.setState(
+                {
+                  entity: e.target.value,
+                  entityLabel: this.__select2Entity.select2('data')[0].text,
+                },
+                () => this._initSelect2(true)
+              )
+            }
           })
         this.__select2Entity = s2entity
         // init
