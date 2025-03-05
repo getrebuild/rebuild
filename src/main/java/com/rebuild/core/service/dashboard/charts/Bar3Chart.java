be6e54a55b9287d12f00e7f42d309fef8d58df14
devezhao <zhaofang123@gmail.com>
2025-03-06 01:23:34 +0800

    rbv: charts

--- src/main/java/com/rebuild/core/service/dashboard/charts/Bar3Chart.java
+++ src/main/java/com/rebuild/core/service/dashboard/charts/Bar3Chart.java
@@ -7,10 +7,7 @@ See LICENSE and COMMERCIAL in the project root for license information.
 
 package com.rebuild.core.service.dashboard.charts;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.rebuild.core.support.i18n.Language;
-import com.rebuild.utils.RbAssert;
 
 /**
  * 柱状图
@@ -22,11 +19,5 @@ public class Bar3Chart extends LineChart {
 
     protected Bar3Chart(JSONObject config) {
         super(config);
-    }
-
-    @Override
-    public JSON build() {
-        RbAssert.isCommercial(Language.L("免费版不支持此图表"));
-        return super.build();
     }
 }
