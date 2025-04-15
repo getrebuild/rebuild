c59e7e0d909f8c68e69ec6eac494bc9892c21a1e
devezhao <zhaofang123@gmail.com>
2025-04-15 21:44:01 +0800

    4.0.3

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -75,11 +75,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "4.0.2";
+    public static final String VER = "4.0.3";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 4000208;
+    public static final int BUILD = 4000309;
 
     static {
         // Driver for DB
