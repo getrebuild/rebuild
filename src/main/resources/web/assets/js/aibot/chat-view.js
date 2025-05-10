372fb073fa1d04f58696158d696a66eb7e821097
RB <42044143+getrebuild@users.noreply.github.com>
2025-05-10 15:25:19 +0800

    be: chat

--- src/main/resources/web/assets/js/aibot/chat-view.js
+++ src/main/resources/web/assets/js/aibot/chat-view.js
@@ -4,6 +4,7 @@ Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights re
 rebuild is dual-licensed under commercial and open source licenses (GPLv3).
 See LICENSE and COMMERCIAL in the project root for license information.
 */
+/* global AiBot */
 
 $(document).ready(() => {
   renderRbcomp(
