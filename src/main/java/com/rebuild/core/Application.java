bbcf932dc65ff8ec8750a6ce363fb201920b06db
RB <42044143+getrebuild@users.noreply.github.com>
2025-05-25 22:05:27 +0800

    hotfix: BUILD

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -80,7 +80,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 4000511;
+    public static final int BUILD = 4000512;
 
     static {
         // Driver for DB
