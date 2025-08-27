b9f4d6a9417632155813d8236511853ca3a4c769
RB <42044143+getrebuild@users.noreply.github.com>
2025-08-27 11:05:49 +0800

    be: 文件真删除

--- src/main/java/com/rebuild/core/service/general/recyclebin/RecycleBinCleanerJob.java
+++ src/main/java/com/rebuild/core/service/general/recyclebin/RecycleBinCleanerJob.java
@@ -8,21 +8,27 @@ See LICENSE and COMMERCIAL in the project root for license information.
 package com.rebuild.core.service.general.recyclebin;
 
 import cn.devezhao.commons.CalendarUtils;
+import cn.devezhao.commons.ObjectUtils;
 import cn.devezhao.persist4j.Entity;
+import cn.devezhao.persist4j.engine.ID;
 import com.rebuild.core.Application;
 import com.rebuild.core.metadata.EntityHelper;
 import com.rebuild.core.metadata.MetadataHelper;
 import com.rebuild.core.support.ConfigurationItem;
 import com.rebuild.core.support.RebuildConfiguration;
 import com.rebuild.core.support.distributed.DistributedJobLock;
+import com.rebuild.core.support.integration.QiniuCloud;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.NamedThreadLocal;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 回收站/变更历史清理
@@ -40,6 +46,7 @@ public class RecycleBinCleanerJob extends DistributedJobLock {
 
         // 回收站
 
+        final Set<String> deletePaths42 = new HashSet<>();
         final int rbDays = RebuildConfiguration.getInt(ConfigurationItem.RecycleBinKeepingDays);
         if (rbDays > 0) {
             log.info("RecycleBin clean running ... {}d", rbDays);
@@ -71,6 +78,15 @@ public class RecycleBinCleanerJob extends DistributedJobLock {
                         refEntity.getField(refRecordIdName).getPhysicalName(),
                         recordIdName, commonFrom);
                 dels.add(delRef);
+
+                // 4.2 删除附件
+                if ("Attachment".equals(refName)) {
+                    String sql = "select FILE_PATH " + delRef.substring(7);
+                    Object[][] array = Application.getQueryFactory().createNativeQuery(sql).array();
+                    for (Object[] o : array) {
+                        deletePaths42.add((String) o[0]);
+                    }
+                }
             }
 
             String delSql = "delete " + commonFrom;
@@ -99,15 +115,53 @@ public class RecycleBinCleanerJob extends DistributedJobLock {
             log.warn("RevisionHistory cleaned : {}", del);
         }
 
-        // CommonLog 保留6个月
+        // CommonLog 保留 180d
 
         Entity entity = MetadataHelper.getEntity(EntityHelper.CommonsLog);
         String delSql = String.format(
                 "delete from `%s` where `%s` < '%s 00:00:00'",
                 entity.getPhysicalName(),
                 entity.getField("logTime").getPhysicalName(),
-                CalendarUtils.getUTCDateFormat().format(CalendarUtils.addMonth(-6)));
+                CalendarUtils.getUTCDateFormat().format(CalendarUtils.addDay(-180)));
         Application.getSqlExecutor().execute(delSql, 60 * 3);
+
+        // 4.2 已删除附件最少保留 180d
+
+        List<ID> deletesAuto42 = new ArrayList<>();
+        Object[][] array = Application.createQueryNoFilter(
+                "select attachmentId,filePath from Attachment where isDeleted = 'T' and modifiedOn < ?")
+                .setParameter(1, CalendarUtils.addDay(-Math.max(rbDays, 180)))
+                .array();
+        for (Object[] o : array) {
+            deletesAuto42.add((ID) o[0]);
+            deletePaths42.add((String) o[1]);
+        }
+
+        // 删记录
+        if (!deletesAuto42.isEmpty()) {
+            Application.getCommonsService().delete(deletesAuto42.toArray(new ID[0]), false);
+        }
+        // 删文件
+        int del = 0;
+        for (String path : deletePaths42) {
+            Object[] o = Application.createQueryNoFilter(
+                    "select count(filePath) from Attachment where filePath = ?")
+                    .setParameter(1, path)
+                    .unique();
+            // 检查附件是否有其他字段使用（例如记录转换、触发器处理的）
+            if (o != null && ObjectUtils.toInt(o[0]) > 0) continue;
+
+            if (QiniuCloud.instance().available()) {
+                try {
+                    boolean s = QiniuCloud.instance().delete(path);
+                    del += s ? 1 : 0;
+                } catch (Exception ignored) {}
+            } else {
+                boolean s = FileUtils.deleteQuietly(RebuildConfiguration.getFileOfData(path));
+                del += s ? 1 : 0;
+            }
+        }
+        log.warn("File/Attachment deleted : {}", del);
     }
 
     // --
