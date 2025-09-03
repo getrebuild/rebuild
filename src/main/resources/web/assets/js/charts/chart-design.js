513507e25eaa3030e321ab391a1bfd60a1b99b0a
RB <42044143+getrebuild@users.noreply.github.com>
2025-09-03 13:55:44 +0800

    Add search functionality to chart design fields

--- src/main/resources/web/assets/js/charts/chart-design.js
+++ src/main/resources/web/assets/js/charts/chart-design.js
@@ -96,6 +96,27 @@ $(document).ready(() => {
       .disableSelection()
   }, 1000)
 
+  // v4.2 搜索
+  $('.search-input input').on('input', (e) => {
+    $setTimeout(
+      () => {
+        const q = $trim(e.target.value).toLowerCase()
+        $('.list-unstyled.fields>li>a').each(function () {
+          const $item = $(this)
+          const pinyin = $item.data('pinyin')
+          if (!q || $item.text().toLowerCase().includes(q) || $item.data('field').toLowerCase().includes(q) || (pinyin && pinyin.toLowerCase().includes(q))) {
+            $item.removeClass('hide')
+          } else {
+            $item.addClass('hide')
+          }
+        })
+        $('.data-aside>.rb-scroller').perfectScrollbar('update')
+      },
+      200,
+      'sortable-box'
+    )
+  })
+
   let _AdvFilter
   $('.J_filter').on('click', (e) => {
     $stopEvent(e, true)
