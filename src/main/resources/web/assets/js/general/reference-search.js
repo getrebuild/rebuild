c297039b751e5a9b601f621dd590a5383e55afd6
RB <42044143+getrebuild@users.noreply.github.com>
2025-11-05 14:00:23 +0800

    Update reference-search.js

--- src/main/resources/web/assets/js/general/reference-search.js
+++ src/main/resources/web/assets/js/general/reference-search.js
@@ -4,6 +4,7 @@ Copyright (c) REBUILD <https://getrebuild.com/> and/or its owners. All rights re
 rebuild is dual-licensed under commercial and open source licenses (GPLv3).
 See LICENSE and COMMERCIAL in the project root for license information.
 */
+/* global AdvFilters */
 
 const wpc = window.__PageConfig
 
@@ -52,57 +53,6 @@ $(document).ready(() => {
     if (ids.length > 0 && parent && parent.referenceSearch__call) parent.referenceSearch__call(ids)
   })
 
-  // v4.1 高级查询
-  const $customAdvWrap = $('#dropdown-menu-advfilter')
-  $(document.body).on('click', (e) => {
-    if (!e.target) return
-    const $target = $(e.target)
-    if (
-      $target.hasClass('J_filterbtn') ||
-      $target.parent().hasClass('J_filterbtn') ||
-      $target.hasClass('dropdown-menu-advfilter') ||
-      $target.parents('.dropdown-menu-advfilter')[0] ||
-      $target.hasClass('modal') ||
-      $target.parents('.modal')[0] ||
-      $target.parents('.select2-container')[0] ||
-      $target.hasClass('select2-selection__choice__remove')
-    ) {
-      return
-    }
-    if (!$customAdvWrap.hasClass('hide')) $customAdvWrap.addClass('hide')
-  })
-
-  let _AdvFilter
-  $('.J_filterbtn').on('click', () => {
-    if (_AdvFilter) {
-      $customAdvWrap.toggleClass('hide')
-    } else {
-      const props = {
-        entity: wpc.entity[0],
-        noSave: true,
-        onConfirm: function () {
-          setTimeout(() => $customAdvWrap.addClass('hide'), 200)
-        },
-      }
-      // eslint-disable-next-line react/jsx-no-undef
-      renderRbcomp(<ListAdvFilter {...props} />, $customAdvWrap, function () {
-        _AdvFilter = this
-      })
-    }
-  })
-
-  // v4.2 已有常用查询
-  $.get(`/app/${wpc.entity[0]}/advfilter/list`, function (res) {
-    const $menu = $('.adv-search .dropdown-menu')
-    res.data.forEach((item) => {
-      const $item = $(`<div class="dropdown-item J_custom" data-id="${item.id}"><a class="text-truncate"></a></div>`).appendTo($menu)
-      $item.find('>a').text(item.name)
-    })
-
-    $menu.find('.dropdown-item').on('click', function () {
-      const $item = $(this)
-      $('.adv-search .J_name').text($item.text())
-      RbListPage._RbList.setAdvFilter($item.data('id'))
-    })
-  })
+  // v4.1, 4.2 高级查询
+  AdvFilters.init('.adv-search', window.__PageConfig.entity[0])
 })
