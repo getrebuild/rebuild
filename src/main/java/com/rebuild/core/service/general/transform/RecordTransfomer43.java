c2ececeecae6903edcee5d61565ecf3c4d44a7f4
RB <42044143+getrebuild@users.noreply.github.com>
2025-12-21 16:48:50 +0800

    fix: one2nModeField

--- src/main/java/com/rebuild/core/service/general/transform/RecordTransfomer43.java
+++ src/main/java/com/rebuild/core/service/general/transform/RecordTransfomer43.java
@@ -10,18 +10,23 @@ package com.rebuild.core.service.general.transform;
 import cn.devezhao.commons.ObjectUtils;
 import cn.devezhao.persist4j.Record;
 import cn.devezhao.persist4j.engine.ID;
-import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.rebuild.core.configuration.general.MultiSelectManager;
 import com.rebuild.core.metadata.MetadataHelper;
 import com.rebuild.core.metadata.easymeta.DisplayType;
 import com.rebuild.core.metadata.easymeta.EasyField;
 import com.rebuild.core.metadata.easymeta.EasyMetaFactory;
+import com.rebuild.core.service.TransactionManual;
 import com.rebuild.core.service.query.QueryHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.transaction.TransactionStatus;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * v4.3 支持 `one2n` 模式
@@ -29,6 +34,7 @@ import java.util.List;
  * @author Zixin
  * @since 2025/11/23
  */
+@Slf4j
 public class RecordTransfomer43 extends RecordTransfomer39 {
 
     private ID sourceRecordId;
@@ -56,7 +62,7 @@ public class RecordTransfomer43 extends RecordTransfomer39 {
         }
 
         // 需要保存为多条
-        // TODO 如果多条主记录，明细（如有）都会带着
+        // 如果多条主记录，明细（如有）都会带着
 
         Object multiValue = QueryHelper.queryFieldValue(sourceRecordId, one2nModeField);
         if (multiValue == null) {
@@ -87,29 +93,51 @@ public class RecordTransfomer43 extends RecordTransfomer39 {
             return super.saveRecord(record, detailsList);
         }
 
+
+
         // `转换依据字段` 未映射则不保存此字段值
-        Object hm = transConfig.getJSONObject("fieldsMapping").get(one2nModeField);
-        DisplayType hasMappingTargetType = null;
-        if (hm != null) {
-            if (hm instanceof JSONArray) {
-                // 使用固定值
-            } else {
-                hasMappingTargetType = EasyMetaFactory.getDisplayType(record.getEntity().getField(hm.toString()));
+        JSONObject fieldsMapping = transConfig.getJSONObject("fieldsMapping");
+        String hmField = null;
+        DisplayType hmType = null;
+        for (Map.Entry<String, Object> e : fieldsMapping.entrySet()) {
+            if (one2nModeField.equals(e.getValue())) {
+                hmField = e.getKey();
+                hmType = EasyMetaFactory.getDisplayType(record.getEntity().getField(hmField));
+                break;
             }
         }
 
         List<ID> theNewId = new ArrayList<>();
-        for (Object value : multiValueSplit) {
-            Record clone = record.clone();
-            if (hasMappingTargetType != null) {
-                if (hasMappingTargetType == DisplayType.N2NREFERENCE) {
-                    clone.setObjectValue(one2nModeField, new ID[]{(ID) value});
-                } else {
-                    clone.setObjectValue(one2nModeField, value);
+        TransactionStatus tx = TransactionManual.newTransaction();
+        try {
+            for (Object value : multiValueSplit) {
+                Record clone = record.clone();
+                if (hmType != null) {
+                    if (hmType == DisplayType.N2NREFERENCE) {
+                        clone.setObjectValue(hmField, new ID[]{(ID) value});
+                    } else {
+                        clone.setObjectValue(hmField, value);
+                    }
                 }
+
+                List<Record> detailsListClone = null;
+                if (CollectionUtils.isNotEmpty(detailsList)) {
+                    detailsListClone = new ArrayList<>();
+                    for (Record d : detailsList) {
+                        detailsListClone.add(d.clone());
+                    }
+                }
+
+                theNewId.add(super.saveRecord(clone, detailsListClone));
             }
 
-            theNewId.add(super.saveRecord(clone, detailsList));
+            TransactionManual.commit(tx);
+
+        } catch (Exception ex) {
+            TransactionManual.rollback(tx);
+
+            log.error("Saving `one2nModeField` error : {}", transid, ex);
+            throw ex;
         }
 
         // 回填
