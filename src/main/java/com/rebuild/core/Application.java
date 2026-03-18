072e73130354513030288f83450d9fb9463ea0bb
RB <42044143+getrebuild@users.noreply.github.com>
2026-03-19 00:26:34 +0800

    Update Application.java

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -81,7 +81,7 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 4030004;
+    public static final int BUILD = 4030005;
 
     static {
         // Driver for DB
