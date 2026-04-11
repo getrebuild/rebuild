54500c796509449eaf258f0bb294560c164a20a9
RB <42044143+getrebuild@users.noreply.github.com>
2026-04-11 15:42:08 +0800

    4.3.4

--- src/main/java/com/rebuild/core/Application.java
+++ src/main/java/com/rebuild/core/Application.java
@@ -77,11 +77,11 @@ public class Application implements ApplicationListener<ApplicationStartedEvent>
     /**
      * Rebuild Version
      */
-    public static final String VER = "4.3.3";
+    public static final String VER = "4.3.4";
     /**
      * Rebuild Build [MAJOR]{1}[MINOR]{2}[PATCH]{2}[BUILD]{2}
      */
-    public static final int BUILD = 4030309;
+    public static final int BUILD = 4030410;
 
     static {
         // Driver for DB
