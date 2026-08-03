d69eda20c2871b9c64d3560718e052fba7be9c14
RB <42044143+getrebuild@users.noreply.github.com>
2026-08-03 15:29:49 +0800

    fix label

--- src/main/resources/web/assets/js/rb-components.js
+++ src/main/resources/web/assets/js/rb-components.js
@@ -998,7 +998,7 @@ class RecordSelector extends React.Component {
     this.__select2 = $initReferenceSelect2(this._$select, {
       searchType: 'search',
       entity: props.entity,
-      placeholder: props.entityLabel || undefined,
+      placeholder: props.placeholder || props.entityLabel || undefined,
     }).on('change', (e) => {
       typeof props.onSelect === 'function' && props.onSelect(e.target.value)
     })
