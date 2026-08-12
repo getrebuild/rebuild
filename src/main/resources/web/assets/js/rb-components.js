e679e71e3d6f404859c9c4abfa9ed5faea832808
RB <getrebuild@sina.com>
2026-08-12 11:39:34 +0800

    be

--- src/main/resources/web/assets/js/rb-components.js
+++ src/main/resources/web/assets/js/rb-components.js
@@ -381,7 +381,7 @@ class RbAlert extends React.Component {
 class RbAlertFree43 extends RbAlert {
   renderContent() {
     return (
-      <div className="text-center">
+      <div className="text-center p-2">
         <div>
           <h4 className="m-0 mb-2 text-bold" style={{ fontSize: '1.538rem', marginTop: -5 }}>
             {$L('升级后使用')}
