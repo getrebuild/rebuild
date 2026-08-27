439d7511c440f02d42c9aac6b494f0b2887a934d
RB <42044143+getrebuild@users.noreply.github.com>
2026-08-27 22:37:47 +0800

    rm: entity-truncate

--- src/main/resources/web/assets/js/metadata/entity-advanced.js
+++ src/main/resources/web/assets/js/metadata/entity-advanced.js
@@ -16,12 +16,11 @@ $(document).ready(() => {
   if (!metaid) {
     $('.J_drop-confirm').next().removeClass('hide')
     $('.J_drop-confirm').remove()
-    $('.J_truncate-confirm').remove()
     $('.J_drop-check').parent().parent().remove()
     return
   }
 
-  const $confirm = $('.J_drop-confirm, .J_truncate-confirm')
+  const $confirm = $('.J_drop-confirm')
   $('.J_drop-check').on('click', function () {
     $confirm.attr('disabled', !$(this).prop('checked'))
   })
@@ -38,27 +37,6 @@ $(document).ready(() => {
           if (res.error_code === 0) {
             RbHighbar.success($L('实体已删除'))
             setTimeout(() => location.replace('../../entities'), 1500)
-          } else {
-            RbHighbar.error(res.error_msg)
-            this.disabled()
-          }
-        })
-      },
-      countdown: 5,
-    })
-  })
-  $('.J_truncate-confirm').on('click', () => {
-    if (!$('.J_drop-check').prop('checked')) return
-    RbAlert.create($L('此操作将直接清空数据，不会保留在回收站及触发相关业务规则。'), $L('清空数据'), {
-      type: 'danger',
-      confirmText: $L('清空'),
-      confirm: function () {
-        $confirm.button('loading')
-        this.disabled(true)
-        $.post(`../entity-truncate?id=${metaid}`, (res) => {
-          if (res.error_code === 0) {
-            RbHighbar.success($L('数据已清空'))
-            setTimeout(() => location.reload(), 1500)
           } else {
             RbHighbar.error(res.error_msg)
             this.disabled()
