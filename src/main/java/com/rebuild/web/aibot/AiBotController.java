a70badd0f023c2999023b0ec8cce6ab1bb6ff58f
RB <42044143+getrebuild@users.noreply.github.com>
2026-09-09 15:33:56 +0800

    chat 自动登录

--- src/main/java/com/rebuild/web/aibot/AiBotController.java
+++ src/main/java/com/rebuild/web/aibot/AiBotController.java
@@ -12,19 +12,22 @@ import cn.devezhao.persist4j.engine.ID;
 import com.rebuild.api.user.AuthTokenManager;
 import com.rebuild.core.Application;
 import com.rebuild.core.UserContextHolder;
+import com.rebuild.core.privileges.UserHelper;
 import com.rebuild.core.support.License;
 import com.rebuild.core.support.i18n.Language;
 import com.rebuild.utils.AppUtils;
-import com.rebuild.web.BaseController;
 import com.rebuild.web.WebConstants;
 import com.rebuild.web.commons.RbvMissingController;
+import com.rebuild.web.user.signup.LoginAction;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static cn.devezhao.commons.web.WebUtils.CURRENT_USER;
 import static com.rebuild.utils.AppUtils.URL_AUTHTOKEN;
@@ -37,24 +40,38 @@ import static com.rebuild.web.WebConstants.AUTH_TOKEN;
 @Slf4j
 @RestController
 @RequestMapping("/aibot")
-public class AiBotController extends BaseController {
+public class AiBotController extends LoginAction {
 
     @GetMapping("chat")
-    public ModelAndView chatIndex(HttpServletRequest request) {
+    public ModelAndView chatIndex(HttpServletRequest request, HttpServletResponse response) {
         if (!License.isCommercial()) {
             return RbvMissingController.errorUnsupported(null);
         }
 
         ID user = AppUtils.getRequestUser(request);
         String authToken;
-        // exchange
+        // token exchange
         if (user == null && (authToken = request.getParameter(URL_AUTHTOKEN)) != null) {
             user = AuthTokenManager.verifyToken(authToken, false, true);
             if (user != null) {
-                UserContextHolder.setUser(user);
                 ServletUtils.setSessionAttribute(request, CURRENT_USER, user);
                 request.setAttribute(AUTH_TOKEN, authToken);
+                UserContextHolder.setUser(user);
                 request.setAttribute(WebConstants.$USER, Application.getUserStore().getUser(user));
+            }
+        }
+
+        // 自动登录（记住登录）
+        if (user == null) {
+            String useAlt = ServletUtils.readCookie(request, CK_AUTOLOGIN);
+            if (StringUtils.isNotBlank(useAlt)) {
+                ID altUser = (ID) Application.getCommonsCache().getx(PREFIX_ALT + useAlt);
+                if (altUser != null && UserHelper.isActive(altUser)) {
+                    loginSuccessed(request, response, altUser, true);
+                    user = altUser;
+                    UserContextHolder.setUser(user);
+                    request.setAttribute(WebConstants.$USER, Application.getUserStore().getUser(user));
+                }
             }
         }
 
